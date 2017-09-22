7588fce71b38fc57e7c67e810e93ff955cf234e8
Rafał Kociński <rafal.kocinski@infinityworks.com>
2017-09-22 11:19:27 +0100

    [RPS-16] Create vanilla Hippo project
    Vanilla project generated from Hippo Maven archetype.

---
+++ site/src/main/java/uk/nhs/digital/ps/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package uk.nhs.digital.ps.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="publicationsystem:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
