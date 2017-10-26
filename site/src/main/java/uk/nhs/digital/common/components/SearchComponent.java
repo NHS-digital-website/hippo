30ac7b12241d0a38a8c4b20b837bae9aa437f85d
ngaitang <ngai.tang@nhs.net>
2017-10-26 09:03:19 +0100

    [RPS-103] Add pagination to search results
    Includes standard next and prev links, in addition to
    first and last links. Set to 10 results per page, with
    the number configurable in console.

--- site/src/main/java/uk/nhs/digital/common/components/SearchComponent.java
+++ site/src/main/java/uk/nhs/digital/common/components/SearchComponent.java
@@ -1,7 +1,5 @@
 package uk.nhs.digital.common.components;
 
-import org.apache.commons.lang.math.NumberUtils;
-import org.hippoecm.hst.component.support.bean.BaseHstComponent;
 import org.hippoecm.hst.content.beans.query.builder.HstQueryBuilder;
 import org.hippoecm.hst.content.beans.query.exceptions.QueryException;
 import org.hippoecm.hst.content.beans.query.HstQuery;
@@ -13,40 +11,51 @@ import org.hippoecm.hst.core.component.HstResponse;
 import org.hippoecm.hst.core.parameters.ParametersInfo;
 import org.hippoecm.hst.core.request.HstRequestContext;
 import org.hippoecm.hst.util.SearchInputParsingUtils;
+import org.onehippo.cms7.essentials.components.EssentialsSearchComponent;
+import org.onehippo.cms7.essentials.components.info.EssentialsListComponentInfo;
+import org.onehippo.cms7.essentials.components.paging.Pageable;
 import uk.nhs.digital.ps.beans.Publication;
 
 import static org.hippoecm.hst.content.beans.query.builder.ConstraintBuilder.constraint;
 import static org.hippoecm.hst.content.beans.query.builder.ConstraintBuilder.or;
 
-@ParametersInfo(type = SearchComponentInfo.class)
-public class SearchComponent extends BaseHstComponent {
+@ParametersInfo(type = EssentialsListComponentInfo.class)
+public class SearchComponent extends EssentialsSearchComponent {
 
     @Override
     public void doBeforeRender(final HstRequest request, final HstResponse response) throws HstComponentException {
 
         HstRequestContext requestContext = request.getRequestContext();
         HippoBean scopeBean = requestContext.getContentBean();
-        SearchComponentInfo info = getComponentParametersInfo(request);
+        final EssentialsListComponentInfo paramInfo = getComponentParametersInfo(request);
         String query = SearchInputParsingUtils.parse(
             getPublicRequestParameter(request, "query"), false);
-        int offset = NumberUtils.toInt(
-            getPublicRequestParameter(request, "offset"), 0);
+        final int pageSize = getPageSize(request, paramInfo);
+        final int currentPageNumber = getCurrentPage(request);
+        final int offset = (currentPageNumber - 1) * pageSize;
+
         HstQueryResult searchResult;
 
         try {
-            searchResult = doSearch(scopeBean, query, offset, info.getLimit());
+            searchResult = doSearch(scopeBean, query, offset, pageSize);
         } catch (QueryException e) {
             throw new HstComponentException(
                 "Exception occurred during creation or execution of HstQuery.", e);
         }
 
-        request.setAttribute("result", searchResult.getHippoBeans());
-        request.setAttribute("totalSize", searchResult.getTotalSize());
-        request.setAttribute("size", searchResult.getSize());
-        request.setAttribute("limit", info.getLimit());
+        final Pageable<?extends HippoBean> pageable;
+
+        pageable = getPageableFactory().createPageable(
+            searchResult.getHippoBeans(),
+            searchResult.getTotalSize(),
+            pageSize,
+            currentPageNumber);
+
+        populateRequest(request, paramInfo, pageable);
+
     }
 
-    private HstQueryResult doSearch(HippoBean scope, String query, int offset, int limit) throws QueryException {
+    private HstQueryResult doSearch(HippoBean scope, String query, int offset, int pageSize) throws QueryException {
         HstQuery hstQuery = HstQueryBuilder.create(scope)
             .ofTypes(Publication.class)
             .where(or(
@@ -54,7 +63,7 @@ public class SearchComponent extends BaseHstComponent {
                 constraint("publicationsystem:Summary").contains(query),
                 constraint(".").contains(query)
             ))
-            .limit(limit)
+            .limit(pageSize)
             .offset(offset)
             .build();
 
