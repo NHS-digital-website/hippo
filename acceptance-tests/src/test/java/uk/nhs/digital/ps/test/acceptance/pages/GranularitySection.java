a16333f601c5379da12d4a1068b69203c51da307
Rafał Kociński <rafal.kocinski@infinityworks.com>
2017-10-30 15:25:23 +0000

    [RPS-132] Prevent saving publication with blank Granularity field
    Removed Granularity from:
    - publication.yaml prototypes so that an empty placeholder doesn't
      get automatically added to newly created documents,
    - from the test 'lorem ipsum' documents.
    
    This is a change missed from the main commit 4517457.

--- acceptance-tests/src/test/java/uk/nhs/digital/ps/test/acceptance/pages/GranularitySection.java
+++ acceptance-tests/src/test/java/uk/nhs/digital/ps/test/acceptance/pages/GranularitySection.java
@@ -3,8 +3,21 @@ package uk.nhs.digital.ps.test.acceptance.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import uk.nhs.digital.ps.test.acceptance.models.Granularity;
 
 public class GranularitySection {
+
+    /**
+     * Targets 'parent' div that _contains_ an 'h3/span' child elements, where the <span> has text 'Granularity'
+     * this is so that further searches can be performed in context of the root element.
+     */
+    private static final String ROOT_ELEMENT_XPATH = XpathSelectors.EDITOR_BODY
+        + "//div[@class='hippo-editor-field' and h3/span[text() = 'Granularity']]";
+
+    /** Targets drop-down's {@code <select>} element. */
+    private static final String DROPDOWN_XPATH = ROOT_ELEMENT_XPATH + "//select[@class='dropdown-plugin']";
+
     private final PageHelper helper;
     private final WebDriver webDriver;
 
@@ -17,15 +30,20 @@ public class GranularitySection {
         helper.executeWhenStable(() -> findAddButton().click());
     }
 
-    private WebElement findRootElement() {
-        // find 'parent' div that _contains_ an 'h3/span' child elements, where the <span> has text 'Granularity'
-        // this is so that further searches can be performed in context of the root element
-        return getWebDriver().findElement(
-            By.xpath(XpathSelectors.EDITOR_BODY + "//div[@class='hippo-editor-field' and h3/span[text() = 'Granularity']]"));
+    public void populateGranularityField(final Granularity granularity) {
+        helper.executeWhenStable(() -> findDropDown().selectByVisibleText(granularity.getDisplayValue()));
+    }
+
+    private Select findDropDown() {
+        return new Select(helper.findElement(By.xpath(DROPDOWN_XPATH)));
     }
 
     private WebElement findAddButton() {
         return helper.findElement(() -> findRootElement().findElement(By.linkText("Add")));
+    }
+
+    private WebElement findRootElement() {
+        return getWebDriver().findElement(By.xpath(ROOT_ELEMENT_XPATH));
     }
 
     private WebDriver getWebDriver() {
