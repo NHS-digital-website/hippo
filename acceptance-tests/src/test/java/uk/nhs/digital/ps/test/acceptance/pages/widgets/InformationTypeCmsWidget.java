0eea0b0d925205180ce415804c4942985c37ff25
ngaitang <ngai.tang@nhs.net>
2017-12-05 15:04:08 +0000

    [RPS-190] Update Information Type to non mandatory

---
+++ acceptance-tests/src/test/java/uk/nhs/digital/ps/test/acceptance/pages/widgets/InformationTypeCmsWidget.java
@@ -0,0 +1,65 @@
+package uk.nhs.digital.ps.test.acceptance.pages.widgets;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import uk.nhs.digital.ps.test.acceptance.models.InformationType;
+import uk.nhs.digital.ps.test.acceptance.pages.PageHelper;
+import uk.nhs.digital.ps.test.acceptance.pages.XpathSelectors;
+
+public class InformationTypeCmsWidget {
+
+    /**
+     * Targets 'parent' div that _contains_ an 'h3/span' child elements, where the <span> has text 'Information Type'
+     * this is so that further searches can be performed in context of the root element.
+     */
+    private static final String ROOT_ELEMENT_XPATH = XpathSelectors.EDITOR_BODY
+        + "//span[text()='Information Type']/ancestor::div[contains(@class, 'hippo-editor-field')]";
+
+    /** Targets drop-down's {@code <select>} element. */
+    private static final String DROPDOWN_XPATH = ROOT_ELEMENT_XPATH + "//select[contains(@class, 'dropdown-plugin')]";
+
+    private final PageHelper helper;
+    private final WebDriver webDriver;
+
+    public InformationTypeCmsWidget(final PageHelper helper, final WebDriver webDriver) {
+        this.helper = helper;
+        this.webDriver = webDriver;
+    }
+
+    public void addInformationTypeField() {
+        // get element
+        WebElement addButton = helper.findElement(By.xpath(ROOT_ELEMENT_XPATH + "//a[contains(@class, 'add-link')]"));
+
+        // scroll to element, to prevent errors like "Other element would receive the click"
+        new Actions(webDriver)
+            .moveToElement(addButton)
+            .moveByOffset(0, 200)
+            .perform();
+
+        // click
+        addButton.click();
+
+        // wait after click for the dropdown to appear
+        helper.findElement(By.xpath(DROPDOWN_XPATH));
+    }
+
+    public void populateInformationTypeField(final InformationType informationType) {
+        findDropDown().selectByVisibleText(informationType.getDisplayName());
+    }
+
+    private Select findDropDown() {
+        return new Select(helper.findElement(By.xpath(DROPDOWN_XPATH)));
+    }
+
+    private WebElement findRootElement() {
+        return getWebDriver().findElement(By.xpath(ROOT_ELEMENT_XPATH));
+    }
+
+    private WebDriver getWebDriver() {
+        return webDriver;
+    }
+
+}
