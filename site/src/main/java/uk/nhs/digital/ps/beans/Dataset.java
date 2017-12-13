d753f2e048e06b617bde829b9265c8dbce2b8409
ngaitang <ngai.tang@nhs.net>
2017-12-13 15:40:10 +0000

    [RPS-203] Protect dataset properties
    Protect dataset getters from illegal access when the parent publication
    flag is set to upcoming.

--- site/src/main/java/uk/nhs/digital/ps/beans/Dataset.java
+++ site/src/main/java/uk/nhs/digital/ps/beans/Dataset.java
@@ -5,40 +5,53 @@ import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerat
 import org.hippoecm.hst.content.beans.Node;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Collection;
+
 import org.hippoecm.hst.content.beans.standard.HippoResourceBean;
 import uk.nhs.digital.ps.beans.structuredText.StructuredText;
+import uk.nhs.digital.ps.site.exceptions.DataRestrictionViolationException;
+
+import static java.util.Arrays.asList;
 
 @HippoEssentialsGenerated(internalName = "publicationsystem:dataset")
 @Node(jcrType = "publicationsystem:dataset")
 public class Dataset extends BaseDocument {
-    @HippoEssentialsGenerated(internalName = "publicationsystem:Title")
+
+    private static final Collection<String> propertiesPermittedWhenUpcoming = asList(
+        PropertyKeys.TITLE,
+        PropertyKeys.SUMMARY,
+        PropertyKeys.NOMINAL_DATE
+    );
+
+    @HippoEssentialsGenerated(internalName = PropertyKeys.TITLE)
     public String getTitle() {
-        return getProperty("publicationsystem:Title");
+        return getPropertyIfPermitted(PropertyKeys.TITLE);
     }
 
-    @HippoEssentialsGenerated(internalName = "publicationsystem:Summary")
+    @HippoEssentialsGenerated(internalName = PropertyKeys.SUMMARY)
     public StructuredText getSummary() {
-        return new StructuredText(getProperty("publicationsystem:Summary"));
+        assertPropertyPermitted(PropertyKeys.SUMMARY);
+        return new StructuredText(getProperty(PropertyKeys.SUMMARY, ""));
     }
 
-    @HippoEssentialsGenerated(internalName = "publicationsystem:NominalDate")
+    @HippoEssentialsGenerated(internalName = PropertyKeys.NOMINAL_DATE)
     public Calendar getNominalDate() {
-        return getProperty("publicationsystem:NominalDate");
+        return getPropertyIfPermitted(PropertyKeys.NOMINAL_DATE);
     }
 
-    @HippoEssentialsGenerated(internalName = "publicationsystem:NextPublicationDate")
+    @HippoEssentialsGenerated(internalName = PropertyKeys.NEXT_PUBLICATION_DATE)
     public Calendar getNextPublicationDate() {
-        return getProperty("publicationsystem:NextPublicationDate");
+        return getPropertyIfPermitted(PropertyKeys.NEXT_PUBLICATION_DATE);
     }
 
-    @HippoEssentialsGenerated(internalName = "publicationsystem:GeographicCoverage")
+    @HippoEssentialsGenerated(internalName = PropertyKeys.GEOGRAPHIC_COVERAGE)
     public String getGeographicCoverage() {
-        return getProperty("publicationsystem:GeographicCoverage");
+        return getPropertyIfPermitted(PropertyKeys.GEOGRAPHIC_COVERAGE);
     }
 
-    @HippoEssentialsGenerated(internalName = "hippotaxonomy:keys")
+    @HippoEssentialsGenerated(internalName = PropertyKeys.TAXONOMY)
     public String[] getKeys() {
-        return getProperty("hippotaxonomy:keys");
+        return getPropertyIfPermitted(PropertyKeys.TAXONOMY);
     }
 
     /*
@@ -49,34 +62,36 @@ public class Dataset extends BaseDocument {
      * Temporary make this getter forward and backward compatible, meaning we can deploy this code without running
      * content migration.
      */
-    @HippoEssentialsGenerated(internalName = "publicationsystem:Files-v2")
+    @HippoEssentialsGenerated(internalName = PropertyKeys.FILES_V2)
     public List<HippoResourceBean> getFiles() {
-        List<HippoResourceBean> attachments = getChildBeansByName("publicationsystem:Files-v2", HippoResourceBean.class);
+        assertPropertyPermitted(PropertyKeys.FILES_V2);
+        List<HippoResourceBean> attachments = getChildBeansByName(PropertyKeys.FILES_V2, HippoResourceBean.class);
 
         if (attachments.isEmpty()) {
-            attachments = getChildBeansByName("publicationsystem:Files", HippoResourceBean.class);
+            attachments = getChildBeansByName(PropertyKeys.FILES, HippoResourceBean.class);
         }
 
         return attachments;
     }
 
     public List<Relatedlink> getResourceLinks() {
-        return getChildBeansByName("publicationsystem:ResourceLinks", Relatedlink.class);
+        assertPropertyPermitted(PropertyKeys.RESOURCE_LINKS);
+        return getChildBeansByName(PropertyKeys.RESOURCE_LINKS, Relatedlink.class);
     }
 
-    @HippoEssentialsGenerated(internalName = "publicationsystem:Granularity")
+    @HippoEssentialsGenerated(internalName = PropertyKeys.GRANULARITY)
     public String[] getGranularity() {
-        return getProperty("publicationsystem:Granularity");
+        return getPropertyIfPermitted(PropertyKeys.GRANULARITY);
     }
 
-    @HippoEssentialsGenerated(internalName = "publicationsystem:CoverageStart")
+    @HippoEssentialsGenerated(internalName = PropertyKeys.COVERAGE_START)
     public Calendar getCoverageStart() {
-        return getProperty("publicationsystem:CoverageStart");
+        return getPropertyIfPermitted(PropertyKeys.COVERAGE_START);
     }
 
-    @HippoEssentialsGenerated(internalName = "publicationsystem:CoverageEnd")
+    @HippoEssentialsGenerated(internalName = PropertyKeys.COVERAGE_END)
     public Calendar getCoverageEnd() {
-        return getProperty("publicationsystem:CoverageEnd");
+        return getPropertyIfPermitted(PropertyKeys.COVERAGE_END);
     }
 
     public Publication getParentPublication() {
@@ -94,5 +109,42 @@ public class Dataset extends BaseDocument {
         }
 
         return publicationBean;
+    }
+
+    private <T> T getPropertyIfPermitted(final String propertyKey) {
+        assertPropertyPermitted(propertyKey);
+
+        return getProperty(propertyKey);
+    }
+
+    private void assertPropertyPermitted(final String propertyKey) {
+
+        final boolean isPropertyPermitted =
+            propertiesPermittedWhenUpcoming.contains(propertyKey) || isPubliclyAccessible();
+
+        if (!isPropertyPermitted) {
+            throw new DataRestrictionViolationException(
+                "Property is not available when parent publication is flagged as 'not publicly accessible': " + propertyKey
+            );
+        }
+    }
+
+    public boolean isPubliclyAccessible() {
+        return getParentPublication().isPubliclyAccessible();
+    }
+
+    interface PropertyKeys {
+        String TITLE = "publicationsystem:Title";
+        String SUMMARY = "publicationsystem:Summary";
+        String TAXONOMY = "hippotaxonomy:keys";
+        String NOMINAL_DATE = "publicationsystem:NominalDate";
+        String NEXT_PUBLICATION_DATE = "publicationsystem:NextPublicationDate";
+        String COVERAGE_START = "publicationsystem:CoverageStart";
+        String COVERAGE_END = "publicationsystem:CoverageEnd";
+        String GEOGRAPHIC_COVERAGE = "publicationsystem:GeographicCoverage";
+        String GRANULARITY = "publicationsystem:Granularity";
+        String RESOURCE_LINKS = "publicationsystem:ResourceLinks";
+        String FILES = "publicationsystem:Files";
+        String FILES_V2 = "publicationsystem:Files-v2";
     }
 }
---
+++ site/src/test/java/uk/nhs/digital/ps/beans/DatasetTest.java
@@ -0,0 +1,209 @@
+package uk.nhs.digital.ps.beans;
+
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import org.hippoecm.hst.container.RequestContextProvider;
+import org.hippoecm.hst.content.beans.manager.ObjectConverter;
+import org.hippoecm.hst.content.beans.query.HstQuery;
+import org.hippoecm.hst.content.beans.query.HstQueryResult;
+import org.hippoecm.hst.content.beans.query.builder.HstQueryBuilder;
+import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.hippoecm.hst.content.beans.standard.HippoFolder;
+import org.hippoecm.hst.core.request.HstRequestContext;
+import org.hippoecm.hst.mock.core.request.MockHstRequestContext;
+import org.hippoecm.hst.provider.jcr.JCRValueProvider;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
+import uk.nhs.digital.ps.site.exceptions.DataRestrictionViolationException;
+import uk.nhs.digital.ps.test.util.ReflectionHelper;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.*;
+
+import static java.util.Arrays.asList;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+
+@RunWith(PowerMockRunner.class)
+@PowerMockIgnore("javax.management.*")
+@PowerMockRunnerDelegate(DataProviderRunner.class)
+@PrepareForTest(HstQueryBuilder.class)
+public class DatasetTest {
+
+    private static final String ATTACHMENTS_PROPERTY_KEY_OLD = "publicationsystem:Files";
+    private static final String ATTACHMENTS_PROPERTY_KEY = "publicationsystem:Files-v2";
+    private static final String RELATED_LINKS_PROPERTY_KEY = "publicationsystem:RelatedLinks";
+    private static final String RESOURCE_LINKS_PROPERTY_KEY = "publicationsystem:ResourceLinks";
+
+    @Mock private JCRValueProvider valueProvider;
+    @Mock private Node node;
+    @Mock private NodeIterator nodeIterator;
+    @Mock private ObjectConverter objectConverter;
+    @Mock private Node folderNode;
+    @Mock private HippoFolder folder;
+    @Mock private Publication publication;
+
+    @Mock private HstRequestContext requestContext;
+    @Mock private HstQueryBuilder queryBuilder;
+    @Mock private HstQuery query;
+    @Mock private HstQueryResult queryResult;
+
+    private final Map<String, Object> beanProperties = new HashMap<>();
+    private Dataset dataset;
+
+    @Before
+    public void setUp() throws Exception {
+        initMocks(this);
+        beanProperties.clear();
+
+        dataset = new Dataset();
+
+        given(valueProvider.getProperties()).willReturn(beanProperties);
+        given(valueProvider.getParentJcrNode()).willReturn(folderNode);
+
+        given(nodeIterator.hasNext()).willReturn(false);
+        given(node.getNodes(ATTACHMENTS_PROPERTY_KEY)).willReturn(nodeIterator);
+        given(node.getNodes(ATTACHMENTS_PROPERTY_KEY_OLD)).willReturn(nodeIterator);
+        given(node.getNodes(RELATED_LINKS_PROPERTY_KEY)).willReturn(nodeIterator);
+        given(node.getNodes(RESOURCE_LINKS_PROPERTY_KEY)).willReturn(nodeIterator);
+
+        // Mock parent publication
+        given(folder.getBean("content", Publication.class)).willReturn(publication);
+        given(objectConverter.getObject(folderNode)).willReturn(folder);
+
+        given(folderNode.getNodes()).willReturn(nodeIterator);
+
+        // Mock this static method so we can simulate the building and execution of a query
+        PowerMockito.mockStatic(HstQueryBuilder.class);
+        PowerMockito.when(HstQueryBuilder.create(folder)).thenReturn(queryBuilder);
+
+        given(queryBuilder.ofTypes(any(Class.class))).willReturn(queryBuilder);
+        given(queryBuilder.orderByDescending(anyString())).willReturn(queryBuilder);
+        given(queryBuilder.build()).willReturn(query);
+        given(query.execute()).willReturn(queryResult);
+
+        ReflectionHelper.callMethod(RequestContextProvider.class, "set", HstRequestContext.class, requestContext);
+
+        ReflectionHelper.setField(dataset, "name", "arbitrary-name");
+        ReflectionHelper.setField(dataset, "valueProvider", valueProvider);
+        ReflectionHelper.setField(dataset, "node", node);
+        ReflectionHelper.setField(dataset, "objectConverter", objectConverter);
+    }
+
+    @Test
+    @UseDataProvider("gettersForbiddenInUpcomingPublication")
+    public void restrictsGetters_whenPublicationNotPubliclyAvailable(
+        final Method forbiddenGetter) throws Exception {
+
+        // given
+        given(publication.isPubliclyAccessible()).willReturn(false);
+
+        try {
+            // when
+            forbiddenGetter.invoke(dataset);
+
+            // then
+            fail("No exception was thrown from getter " + forbiddenGetter);
+
+        } catch (final InvocationTargetException e) {
+            assertThat("Getter " + forbiddenGetter + " throws exception of correct type.", e.getCause(),
+                instanceOf(DataRestrictionViolationException.class)
+            );
+            assertThat("Getter " + forbiddenGetter + " throws exception with correct message.", e.getCause().getMessage(),
+                startsWith("Property is not available when parent publication is flagged as 'not publicly accessible':")
+            );
+        } catch (final Exception e) {
+            System.out.println(e.getMessage());
+            throw new Error("Failed to test '" + forbiddenGetter + " because of an error;" +
+                " see stack trace below for details.", e
+            );
+        }
+    }
+
+    @Test
+    @UseDataProvider("allPublicGetters")
+    public void permitsAllGetters_whenPublicationFlaggedAsPubliclyAvailable(final Method permittedGetter) throws Exception {
+
+        // given
+        given(publication.isPubliclyAccessible()).willReturn(true);
+
+        // some getters have arguments so we need to construct mocked instances and pass them in as parameters
+        Object[] args = Arrays.stream(permittedGetter.getParameterTypes())
+            .map(DatasetTest::newInstanceForClass)
+            .toArray();
+
+        try {
+            // when
+            permittedGetter.invoke(dataset, args);
+
+            // then
+            // pass
+        } catch (final Throwable e) {
+            e.printStackTrace(); // helps with debugging
+            throw new AssertionError(
+                "No exception was expected to be thrown from '" + permittedGetter + "' but one was emitted;" +
+                    " see stack trace below for details.", e
+            );
+        }
+    }
+
+    /**
+     * Create an instance of a class that we can pass as a parameter in our calls to the getters
+     * Currently only implemented for HstRequestContext
+     */
+    private static <R> R newInstanceForClass(Class<R> cls) {
+        if (cls.equals(HstRequestContext.class)) {
+            return (R) new MockHstRequestContext();
+        } else {
+            throw new RuntimeException("Don't know how to construct class " + cls);
+        }
+    }
+
+    @DataProvider
+    public static List<Method> allPublicGetters() {
+
+        return stream(Dataset.class.getDeclaredMethods())
+            .filter(method -> Modifier.isPublic(method.getModifiers()))
+            .filter(method -> !Modifier.isStatic(method.getModifiers()))
+            .filter(method -> method.getName().startsWith("get") || method.getName().startsWith("is"))
+            .filter(method -> !method.getReturnType().equals(Void.class))
+            .collect(toList());
+    }
+
+    @DataProvider
+    public static List<Method> gettersForbiddenInUpcomingPublication() {
+
+        final List<String> gettersPermittedWhenUpcoming = asList(
+            "getTitle",
+            "getSummary",
+            "getNominalDate",
+            "getParentPublication",
+            "isPubliclyAccessible",
+            "getSelfLinkBean"
+        );
+
+        return allPublicGetters().stream()
+            .filter(method -> !gettersPermittedWhenUpcoming.contains(method.getName()))
+            .collect(toList());
+    }
+}
