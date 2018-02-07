18f6a15ce40dd8370fd11f2bc53941db2897d979
Rafał Kociński <rafal.kocinski@infinityworks.com>
2018-02-07 12:00:59 +0000

    [RPS-374] Extract Migrator to a dedicated git repo
    Removed entire 'migrator' module and moved it into newly created git
    repo:
    
    https://github.com/NHS-digital-website/indicators-hippo-migrator
    
    Also tiny fix for deploy pileline

--- migrator/src/main/java/uk/nhs/digital/ps/migrator/model/hippo/Folder.java
+++
@@ -1,9 +0,0 @@
-package uk.nhs.digital.ps.migrator.model.hippo;
-
-public class Folder extends HippoImportableItem {
-
-    public Folder(final Folder parentFolder, final String displayName) {
-        super(parentFolder, displayName);
-    }
-
-}
