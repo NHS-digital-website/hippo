d5567c96d7ac2243c45cb4f922624ae1dafaff01
Rob Pataki <robert.pataki@valtech.co.uk>
2018-02-26 18:14:38 +0100

    [DW-23] Add List document type
    Also includes UI implementation of first pass [DW-26]

---
+++ site/src/main/java/uk/nhs/digital/website/beans/Externallink.java
@@ -0,0 +1,30 @@
+package uk.nhs.digital.website.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoCompound;
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+
+@HippoEssentialsGenerated(internalName = "website:externallink")
+@Node(jcrType = "website:externallink")
+public class Externallink extends HippoCompound {
+
+    // used to differentiate between different types of content blocks
+    public String getType() {
+        return "external";
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:link")
+    public String getLink() {
+        return getProperty("website:link");
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:title")
+    public String getTitle() {
+        return getProperty("website:title");
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:shortsummary")
+    public String getShortsummary() {
+        return getProperty("website:shortsummary");
+    }
+}
---
+++ site/src/main/java/uk/nhs/digital/website/beans/Internallink.java
@@ -0,0 +1,21 @@
+package uk.nhs.digital.website.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.hippoecm.hst.content.beans.standard.HippoCompound;
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+
+@HippoEssentialsGenerated(internalName = "website:internallink")
+@Node(jcrType = "website:internallink")
+public class Internallink extends HippoCompound {
+
+    // used to differentiate between different types of content blocks
+    public String getType() {
+        return "internal";
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:link")
+    public HippoBean getLink() {
+        return getLinkedBean("website:link", HippoBean.class);
+    }
+}
