03fce2ffc83a121065aa6fe96f9c61ce9c38bbad
Rob Pataki <robert.pataki@valtech.co.uk>
2018-02-26 18:14:38 +0100

    [DW-23] Add List document type
    Also includes UI implementation of first pass [DW-26]

---
+++ site/src/main/java/uk/nhs/digital/website/beans/ComponentList.java
@@ -0,0 +1,46 @@
+package uk.nhs.digital.website.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+
+import java.util.*;
+
+@HippoEssentialsGenerated(internalName = "website:componentlist")
+@Node(jcrType = "website:componentlist")
+public class ComponentList extends BaseDocument {
+    @HippoEssentialsGenerated(internalName = "website:anchor")
+    public Boolean getAnchor() {
+        return getProperty("website:anchor");
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:seosummary")
+    public String getSeosummary() {
+        return getProperty("website:seosummary");
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:shortsummary")
+    public String getShortsummary() {
+        return getProperty("website:shortsummary");
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:summary")
+    public String getSummary() {
+        return getProperty("website:summary");
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:title")
+    public String getTitle() {
+        return getProperty("website:title");
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:body")
+    public HippoHtml getBody() {
+        return getHippoHtml("website:body");
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:items")
+    public List<?> getBlocks() {
+        return getChildBeansByName("website:items");
+    }
+}
