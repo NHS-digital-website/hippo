12f1ba7db553f00627113ead1a9db4740c2b0204
Jon McHale <jon.mchale@infinityworks.com>
2018-02-28 13:30:33 +0000

    [NIL-98] Add resources to Indicator documenet type

--- site/src/main/java/uk/nhs/digital/nil/beans/Indicator.java
+++ site/src/main/java/uk/nhs/digital/nil/beans/Indicator.java
@@ -3,17 +3,31 @@ package uk.nhs.digital.nil.beans;
 import org.hippoecm.hst.content.beans.Node;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 
+import uk.nhs.digital.ps.beans.Attachment;
+
+import java.util.List;
 
 @HippoEssentialsGenerated(internalName = "nationalindicatorlibrary:indicator")
 @Node(jcrType = "nationalindicatorlibrary:indicator")
 public class Indicator extends BaseDocument {
-    @HippoEssentialsGenerated(internalName = "nationalindicatorlibrary:topbar")
+    @HippoEssentialsGenerated(internalName = PropertyKeys.TOPBAR)
     public Topbar getTopbar() {
-        return getBean("nationalindicatorlibrary:topbar", Topbar.class);
+        return getBean(PropertyKeys.TOPBAR, Topbar.class);
     }
 
-    @HippoEssentialsGenerated(internalName = "nationalindicatorlibrary:details")
+    @HippoEssentialsGenerated(internalName = PropertyKeys.DETAILS)
     public Details getDetails() {
-        return getBean("nationalindicatorlibrary:details", Details.class);
+        return getBean(PropertyKeys.DETAILS, Details.class);
+    }
+
+    @HippoEssentialsGenerated(internalName = PropertyKeys.ATTACHMENTS)
+    public List<Attachment> getAttachments() {
+        return getChildBeansByName(PropertyKeys.ATTACHMENTS, Attachment.class);
+    }
+
+    interface PropertyKeys {
+        String ATTACHMENTS = "nationalindicatorlibrary:attachments";
+        String DETAILS = "nationalindicatorlibrary:details";
+        String TOPBAR = "nationalindicatorlibrary:topbar";
     }
 }
