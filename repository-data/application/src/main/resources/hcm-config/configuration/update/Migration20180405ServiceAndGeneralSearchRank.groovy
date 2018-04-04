aa9036650e2750ad8cfc2d90e516d3addca04382
Giacomo Lamonaco <giacomo.lamonaco@bloomreach.com>
2018-04-04 14:37:33 +0200

    [DW-222] Boosting services and general document nodes

---
+++ repository-data/application/src/main/resources/hcm-config/configuration/update/Migration20180405ServiceAndGeneralSearchRank.groovy
@@ -0,0 +1,50 @@
+package org.hippoecm.frontend.plugins.cms.admin.updater
+
+import org.hippoecm.repository.util.JcrUtils
+import org.onehippo.repository.update.BaseNodeUpdateVisitor
+
+import javax.jcr.Node
+
+/**
+ * This migration causes the search rank to be set to 1 to push these docs to the top of
+ * 'relevance' searches
+ */
+class Migration20180315SearchRank extends BaseNodeUpdateVisitor {
+
+    private static final String PROPERTY_SEARCH_RANK = "common:searchRank"
+
+    boolean doUpdate(Node node) {
+        try {
+            log.info("attempting to migrate node: " + node.getPath())
+
+            int value = parametersMap.get("searchRankValue") as int
+
+            JcrUtils.ensureIsCheckedOut(node)
+            node.setProperty(PROPERTY_SEARCH_RANK, value)
+
+            return true
+        } catch (e) {
+            log.error("Failed to process record.", e)
+        }
+
+        return false
+    }
+
+    boolean undoUpdate(Node node) {
+        try {
+            log.info("attempting to undo migration for node: " + node.getPath())
+
+            JcrUtils.ensureIsCheckedOut(node)
+            if (node.hasProperty(PROPERTY_SEARCH_RANK)) {
+                node.getProperty(PROPERTY_SEARCH_RANK).remove()
+                log.info("removed property " + PROPERTY_SEARCH_RANK)
+            }
+
+            return true
+        } catch (e) {
+            log.error("Failed to (undo) process record.", e)
+        }
+
+        return false
+    }
+}
