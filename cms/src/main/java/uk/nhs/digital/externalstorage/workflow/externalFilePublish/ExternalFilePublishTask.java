e50856a2091ffb97d7bd516d6e30193ad479aaf9
Rafał Kociński <rafal.kocinski@infinityworks.com>
2018-04-11 16:09:37 +0100

    [RPS-450] S3 upload cap fix
    Fixes an NPE in workflow publish and unpublish external resource
    tasks.
    
    Workflow Task classes were getting null instead of expected instance
    of S3Connector. This was due to the fact that the previous changes of
    RPS-450 story stopped registering S3Connector in Hippo service registry,
    registering an instance of SchedulingS3Connector instead; the affected
    workflow Action and Task classes were not updated to start using the
    replacement interface.

--- cms/src/main/java/uk/nhs/digital/externalstorage/workflow/externalFilePublish/ExternalFilePublishTask.java
+++ cms/src/main/java/uk/nhs/digital/externalstorage/workflow/externalFilePublish/ExternalFilePublishTask.java
@@ -1,11 +1,11 @@
 package uk.nhs.digital.externalstorage.workflow.externalFilePublish;
 
-import uk.nhs.digital.externalstorage.s3.S3Connector;
+import uk.nhs.digital.externalstorage.s3.SchedulingS3Connector;
 import uk.nhs.digital.externalstorage.workflow.AbstractExternalFileTask;
 
 public class ExternalFilePublishTask extends AbstractExternalFileTask {
 
-    protected void setTargetStatus(S3Connector s3Connector, final String resource) {
+    protected void setTargetStatus(final SchedulingS3Connector s3Connector, final String resource) {
         s3Connector.publishResource(resource);
     }
 }
