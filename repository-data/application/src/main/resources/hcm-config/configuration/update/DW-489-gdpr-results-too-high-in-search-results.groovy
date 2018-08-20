a506e064159cd493e3dda7465b7405b2e37d1d55
ngaitang <ngai.tang@nhs.net>
2018-08-20 13:48:39 +0100

    [DW-489] DW-510 Reduce GDPR searchRank and update old LinkList docs
    Add groovy script to reduce common:searchRank property of
    GDPR transparency documents from 5 to 2 as these were incorrectly
    set on mass import. The GDPR transparency document type is already
    correctly set to 2.
    
    DW-510 script to update old Links List doc types to include
    search rank property

---
+++ repository-data/application/src/main/resources/hcm-config/configuration/update/DW-489-gdpr-results-too-high-in-search-results.groovy
@@ -0,0 +1,66 @@
+package org.hippoecm.frontend.plugins.cms.admin.updater
+
+import org.hippoecm.repository.util.JcrUtils
+import org.onehippo.repository.update.BaseNodeUpdateVisitor
+
+import javax.jcr.Node
+import javax.jcr.NodeIterator
+import javax.jcr.Session
+import javax.jcr.Value
+
+/**
+ * Update GDPR transparency documents, changing their
+ * common:searchRank from 5 to 2.
+ */
+class GdprDocumentsTooHighInSearchResults extends BaseNodeUpdateVisitor {
+
+    private static final String COMMON_SEARCHRANK_PROPERTY = "common:searchRank"
+    Session session
+
+    void initialize(Session session) {
+        this.session = session
+    }
+
+    boolean doUpdate(Node node) {
+
+        // Query returns the hippo:handle node for the document
+        // (which has the 3 variants)
+        try {
+            if (node.hasNodes()) {
+                return updateFieldAndSaveDocument(node)
+            }
+        } catch (e) {
+            log.error("Failed to process record.", e)
+        }
+
+        return false
+    }
+
+
+    boolean updateFieldAndSaveDocument(Node parentHippoHandleNode) {
+
+        NodeIterator nodeIterator = parentHippoHandleNode.getNodes()
+        int variantsUpdated = 0
+
+        // Iterate through each variant
+        while(nodeIterator.hasNext()) {
+            Node n = nodeIterator.nextNode()
+
+            log.info("attempting to update node: " + n.getPath())
+
+            JcrUtils.ensureIsCheckedOut(n)
+            if (n.hasProperty(COMMON_SEARCHRANK_PROPERTY)) {
+                n.getProperty(COMMON_SEARCHRANK_PROPERTY).remove()
+            }
+            n.setProperty(COMMON_SEARCHRANK_PROPERTY, 2l)
+
+            variantsUpdated += 1
+        }
+
+        return variantsUpdated > 0
+    }
+
+    boolean undoUpdate(Node node) {
+        throw new UnsupportedOperationException();
+    }
+}
