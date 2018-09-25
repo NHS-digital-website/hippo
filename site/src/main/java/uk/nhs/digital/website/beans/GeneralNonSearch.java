62a0064f07d28ccff0b534493cc4fcc1169ba120
ngaitang <ngai.tang@nhs.net>
2018-09-25 15:58:59 +0100

    [DW-0] Add duplicate general doc type

---
+++ site/src/main/java/uk/nhs/digital/website/beans/GeneralNonSearch.java
@@ -0,0 +1,10 @@
+package uk.nhs.digital.website.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+
+@HippoEssentialsGenerated(internalName = "website:generalnonsearch")
+@Node(jcrType = "website:generalnonsearch")
+public class GeneralNonSearch extends General {
+
+}
