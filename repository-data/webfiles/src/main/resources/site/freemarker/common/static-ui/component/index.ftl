f5e7e4cd72e3f52567410a105ce4b7b7fba65c8d
ngaitang <ngai.tang@nhs.net>
2019-04-16 08:38:34 +0100

    [DW-0] Revert keyfacts infographics

--- repository-data/webfiles/src/main/resources/site/freemarker/common/static-ui/component/index.ftl
+++ repository-data/webfiles/src/main/resources/site/freemarker/common/static-ui/component/index.ftl
@@ -22,15 +22,6 @@
 
 <article class="hub-box hub-box--light">
     <div class="hub-box__contents">
-        <h2 class="hub-box__title"><a href="/static-ui/component/infographic">Infographic</a></h2>
-        <span class="hub-box__meta">Created February 2019</span>
-        <p class="hub-box__text">Infographics. All icons are svg.</p>
-    </div>
-</article>
-
-
-<article class="hub-box hub-box--light">
-    <div class="hub-box__contents">
         <h2 class="hub-box__title"><a href="/static-ui/component/skip-link">Skip link</a></h2>
         <span class="hub-box__meta">Created January 2019</span>
         <p class="hub-box__text">Use the skip link component to help keyboard-only users skip to the main content on a page.</p>
--- site/src/main/java/uk/nhs/digital/ps/beans/Publication.java
+++ site/src/main/java/uk/nhs/digital/ps/beans/Publication.java
@@ -1,10 +1,8 @@
 package uk.nhs.digital.ps.beans;
 
 import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoHtml;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 import uk.nhs.digital.ps.beans.structuredText.StructuredText;
-import uk.nhs.digital.website.beans.Infographic;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,32 +25,9 @@ public class Publication extends PublicationBase {
         return new StructuredText(getProperty(PublicationBase.PropertyKeys.KEY_FACTS, ""));
     }
 
-    @HippoEssentialsGenerated(internalName = PublicationBase.PropertyKeys.KEY_FACTS_HEAD)
-    public HippoHtml getKeyFactsHead() {
-        assertPropertyPermitted(PublicationBase.PropertyKeys.KEY_FACTS_HEAD);
-
-        return getHippoHtml(PublicationBase.PropertyKeys.KEY_FACTS_HEAD);
-    }
-
     @HippoEssentialsGenerated(internalName = PublicationBase.PropertyKeys.KEY_FACT_IMAGES)
     public List<ImageSection> getKeyFactImages() {
-        assertPropertyPermitted(PropertyKeys.KEY_FACT_IMAGES);
-
         return getChildBeansIfPermitted(PropertyKeys.KEY_FACT_IMAGES, ImageSection.class);
-    }
-
-    @HippoEssentialsGenerated(internalName = PropertyKeys.KEY_FACT_INFOGRAPHICS)
-    public List<Infographic> getKeyFactInfographics() {
-        assertPropertyPermitted(PropertyKeys.KEY_FACT_INFOGRAPHICS);
-
-        return getChildBeansIfPermitted(PropertyKeys.KEY_FACT_INFOGRAPHICS, Infographic.class);
-    }
-
-    @HippoEssentialsGenerated(internalName = PublicationBase.PropertyKeys.KEY_FACTS_TAIL)
-    public HippoHtml getKeyFactsTail() {
-        assertPropertyPermitted(PublicationBase.PropertyKeys.KEY_FACTS_HEAD);
-
-        return getHippoHtml(PublicationBase.PropertyKeys.KEY_FACTS_TAIL);
     }
 
     public List<PublicationPage> getPages() {
