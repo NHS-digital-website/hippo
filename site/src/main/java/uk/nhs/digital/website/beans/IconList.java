c08b72c866067a79c28af6ca42d084ad82902e0c
ngaitang <ngai.tang@nhs.net>
2019-04-29 12:02:46 +0100

    [DW-828] Additional fields to iconlist compound type
    Add HeadingLevel and Introduction fields to IconList.
    Ensure IconList title also appears on side nav.

--- site/src/main/java/uk/nhs/digital/website/beans/IconList.java
+++ site/src/main/java/uk/nhs/digital/website/beans/IconList.java
@@ -2,6 +2,7 @@ package uk.nhs.digital.website.beans;
 
 import org.hippoecm.hst.content.beans.Node;
 import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 
 import java.util.List;
@@ -14,6 +15,16 @@ public class IconList extends BaseCompound {
     @HippoEssentialsGenerated(internalName = "website:title")
     public String getTitle() {
         return getProperty("website:title");
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:headinglevel")
+    public String getHeadingLevel() {
+        return getProperty("website:headinglevel");
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:introduction")
+    public HippoHtml getIntroduction() {
+        return getHippoHtml("website:introduction");
     }
 
     @HippoEssentialsGenerated(internalName = "website:iconlistitems")
