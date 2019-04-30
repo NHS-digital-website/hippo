0ea96375823b2daf4ab4eedbafec499787d45c35
GeoffHayward <geoff@geoffhayward.eu>
2019-04-30 16:04:22 +0100

    [DW-832] Adds server side SVG colour changing

---
+++ site/src/main/java/uk/nhs/digital/svg/colour/SvgColourSetter.java
@@ -0,0 +1,21 @@
+package uk.nhs.digital.svg.colour;
+
+import static java.text.MessageFormat.format;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SvgColourSetter {
+
+    static final Pattern regex = Pattern.compile("#((?:[0-9a-fA-F]{3}){1,2})", Pattern.UNICODE_CASE);
+
+    static String replaceColour(final String input, final String colour) {
+        Matcher regexMatcher = regex.matcher(input);
+        return regexMatcher.replaceAll(colour);
+    }
+
+    public static boolean isColour(String colour) {
+        return format("#{0}", colour).matches(regex.toString());
+    }
+
+}
