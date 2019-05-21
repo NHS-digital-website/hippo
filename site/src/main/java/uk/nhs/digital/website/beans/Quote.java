6a759fb06cdd8da29e76d67f67a00ee4a3fa0a38
ngmode <amit.nagmode@nhs.net>
2019-05-21 15:22:30 +0100

    [DW-465] Backend and ui changes for quote compound

---
+++ site/src/main/java/uk/nhs/digital/website/beans/Quote.java
@@ -0,0 +1,37 @@
+package uk.nhs.digital.website.beans;
+
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoCompound;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
+
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+
+@HippoEssentialsGenerated(internalName = "website:quote")
+@Node(jcrType = "website:quote")
+public class Quote extends HippoCompound {
+
+    @HippoEssentialsGenerated(internalName = "website:quote")
+    public HippoHtml getQuote() {
+        return getHippoHtml("website:quote");
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:person")
+    public String getPerson() {
+        return getProperty("website:person");
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:role")
+    public String getRole() {
+        return getProperty("website:role");
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:organisation")
+    public String getOrganisation() {
+        return getProperty("website:organisation");
+    }
+
+    public String getSectionType() {
+        return "quote";
+    }
+}
