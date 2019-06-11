b2a624740ee9d51cf4805e61aec379a3147bfc91
GeoffHayward <geoff@geoffhayward.eu>
2019-06-11 10:56:27 +0100

    [DW-853] Update to CKEditor Configuration
    - Blacklists depreacted tags
    - Removes the merge options from the context menu as creates rowspan
      and colspan attributes
    - Adds cell propity config

---
+++ cms/src/main/resources/ckeditor/extraConfiguration.js
@@ -0,0 +1,53 @@
+CKEDITOR.on('instanceReady', function(e) {
+    // Remove the 'Merge' option (rowspan & colspan) from the context menu
+    e.editor.removeMenuItem("tablecell_merge");
+    e.editor.removeMenuItem("tablecell_merge_down");
+    e.editor.removeMenuItem("tablecell_merge_right");
+});
+
+CKEDITOR.on('dialogDefinition', function (e) {
+    if (e.data.name == "cellProperties") {
+        e.data.definition.minHeight = 50;
+        e.data.definition.getContents("info").elements[0] = {
+            "type": "hbox",
+            "widths": ["40%", "5%", "40%"],
+            "children": [{
+                "type": "vbox",
+                "padding": 0,
+                "children": [{
+                    "type": "select",
+                    "id": "hAlign",
+                    "label": "Horizontal Alignment",
+                    "default": "",
+                    "items": [
+                        ["<not set>", ""],
+                        ["Left", "left"],
+                        ["Center", "center"],
+                        ["Right", "right"],
+                        ["Justify", "justify"]
+                    ]
+                }]
+            }, {
+                "isSpacer": true,
+                "type": "html",
+                "html": "&nbsp;"
+            }, {
+                "type": "vbox",
+                "padding": 0,
+                "children": [{
+                    "type": "select",
+                    "id": "vAlign",
+                    "label": "Vertical Alignment",
+                    "default": "",
+                    "items": [
+                        ["<not set>", ""],
+                        ["Top", "top"],
+                        ["Middle", "middle"],
+                        ["Bottom", "bottom"],
+                        ["Baseline", "baseline"]
+                    ]
+                }]
+            }]
+        };
+    }
+});
