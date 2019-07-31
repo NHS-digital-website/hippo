722ba6462fd1d02784b70f5c153740f3aa6abe00
ngmode <amit.nagmode@nhs.net>
2019-07-31 09:14:08 +0100

    [DW-833] Backend improvement to Roadmap doctype

---
+++ site/src/main/java/uk/nhs/digital/website/beans/Category.java
@@ -0,0 +1,34 @@
+package uk.nhs.digital.website.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.hippoecm.hst.content.beans.standard.HippoCompound;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+
+import java.util.List;
+
+
+@HippoEssentialsGenerated(internalName = "website:category")
+@Node(jcrType = "website:category")
+public class Category extends HippoCompound {
+
+    @HippoEssentialsGenerated(internalName = "website:name")
+    public String getName() {
+        return getProperty("website:name");
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:description")
+    public HippoHtml getDescription() {
+        return getHippoHtml("website:description");
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:linkservices")
+    public List<HippoBean> getLinkservices() {
+        return getLinkedBeans("website:linkservices", HippoBean.class);
+    }
+
+    public String getSectionType() {
+        return "category";
+    }
+}
