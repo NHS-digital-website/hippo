6c5d6cca95489a319f99c1e2708e47400def4b73
Damian.Ziobro <damian.ziobro1@nhs.net>
2019-09-03 14:59:29 +0100

    [DW-983] Add 'Defined Terms' ckeditor plugin

---
+++ cms/src/main/resources/ckeditor/plugins/definedterms/plugin.js
@@ -0,0 +1,45 @@
+
+CKEDITOR.plugins.add( 'definedterms', {
+
+	icons: 'definedterms',
+
+	init: function( editor ) {
+
+    editor.addContentsCss( this.path + 'styles/definedterms.css' );
+
+		editor.addCommand( 'definedterms', new CKEDITOR.dialogCommand( 'definedtermsDialog', {
+
+			allowedContent: 'span[class]',
+			requiredContent: 'span[class]',
+
+			contentForms: [
+				'term',
+			]
+		} ) );
+
+
+		editor.ui.addButton( 'DefinedTerms', {
+			label: 'The term',
+			command: 'definedterms',
+			toolbar: 'insert'
+		});
+
+		if ( editor.contextMenu ) {
+
+			editor.addMenuGroup( 'definedtermsGroup' );
+			editor.addMenuItem( 'definedtermsItem', {
+				label: 'The term',
+				icon: this.path + 'icons/definedterms.png',
+				command: 'definedterms',
+				group: 'definedtermsGroup'
+			});
+
+			editor.contextMenu.addListener( function( element ) {
+					return { definedtermsItem: CKEDITOR.TRISTATE_OFF };
+			});
+		}
+
+
+		CKEDITOR.dialog.add( 'definedtermsDialog', this.path + 'dialogs/dialog.js' );
+	}
+});
