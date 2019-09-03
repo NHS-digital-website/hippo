128708053b48e4e2378539aa052ce90e7bf303ff
GeoffHayward <geoff@geoffhayward.eu>
2019-09-03 16:41:28 +0100

    [DW-1009] Adds other section types to publication sticky nav

--- site/src/main/java/uk/nhs/digital/ps/components/PublicationPageComponent.java
+++ site/src/main/java/uk/nhs/digital/ps/components/PublicationPageComponent.java
@@ -1,15 +1,20 @@
 package uk.nhs.digital.ps.components;
 
+import static java.util.stream.Collectors.toList;
+
 import org.apache.commons.lang.StringUtils;
+import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.hippoecm.hst.core.component.HstComponentException;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
 import org.onehippo.cms7.essentials.components.EssentialsContentComponent;
+import uk.nhs.digital.ps.beans.ChartSection;
+import uk.nhs.digital.ps.beans.MapSection;
 import uk.nhs.digital.ps.beans.PublicationPage;
 import uk.nhs.digital.ps.beans.TextSection;
+import uk.nhs.digital.website.beans.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class PublicationPageComponent extends EssentialsContentComponent {
 
@@ -27,9 +32,39 @@ public class PublicationPageComponent extends EssentialsContentComponent {
 
     private List<String> getIndex(PublicationPage page) {
         return page.getBodySections().stream()
-            .filter(section -> section instanceof TextSection)
-            .map(section -> ((TextSection) section).getHeading())
+            .map(PublicationPageComponent::getTitle)
             .filter(StringUtils::isNotBlank)
-            .collect(Collectors.toList());
+            .collect(toList());
+    }
+
+    private static String getTitle(HippoBean section) {
+        if (section instanceof Section) {
+            return ((Section) section).getTitle();
+        }
+        if (section instanceof TextSection) {
+            return ((TextSection) section).getHeading();
+        }
+        if (section instanceof ChartSection) {
+            return ((ChartSection) section).getTitle();
+        }
+        if (section instanceof MapSection) {
+            return ((MapSection) section).getTitle();
+        }
+        if (section instanceof IconList) {
+            return ((IconList) section).getTitle();
+        }
+        if (section instanceof GallerySection) {
+            return ((GallerySection) section).getTitle();
+        }
+        if (section instanceof Download) {
+            return ((Download) section).getTitle();
+        }
+        if (section instanceof Expander) {
+            return ((Expander) section).getHeading();
+        }
+        if (section instanceof Infographic) {
+            return ((Infographic) section).getHeadline();
+        }
+        return "";
     }
 }
