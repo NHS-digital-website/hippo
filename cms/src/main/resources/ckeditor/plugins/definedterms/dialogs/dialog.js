c4892e911c01e4cf5a24c4cb3c36be4efc9b043e
Damian.Ziobro <damian.ziobro1@nhs.net>
2019-09-23 13:45:30 +0100

    [DW-1025] Implement dynamically added defined terms

--- cms/src/main/resources/ckeditor/plugins/definedterms/dialogs/dialog.js
+++ cms/src/main/resources/ckeditor/plugins/definedterms/dialogs/dialog.js
@@ -25,6 +25,8 @@ CKEDITOR.dialog.add('definedtermsDialog', function(editor) {
             {
                 id: 'tab-id',
                 label: 'Add new term',
+                requiredContent: 'span[class]',
+                allowedContent: 'span[class]{*}',
 
                 elements: [ {
                     type: 'select',
@@ -32,15 +34,27 @@ CKEDITOR.dialog.add('definedtermsDialog', function(editor) {
                     label: 'Choose a term',
                     items: [['-', '-']],
 
-                    setup: function(element) {
+                    setup: function(element, terms) {
                           element.setAttribute("class", 'definedterm-' + slugify(this.getValue()));
+                          if(this.getValue() in terms) {
+                            colorrgb = terms[this.getValue()];
+                            if (! colorrgb.startsWith("#")) { colorrgb = "#" + colorrgb; }
+                            //validate whether color is in hex format #xxxxxx
+                            //using regular expression
+                            if (colorrgb.search(/#[0-9a-f]{6}$/i) != -1) {
+                              element.setStyle("color", colorrgb);
+                              element.setStyle("border-color", colorrgb);
+                            } else {
+                              element.removeStyle("color");
+                              element.removeStyle("border-color");
+                            }
+                          }
                           element.setHtml(this.getValue());
                           this.setValue(element.getHtml());
                         },
 
-                    commit: function(element) {
-                      element.setAttribute("class", 'definedterm-' + slugify(this.getValue()));
-                      element.setHtml(this.getValue());
+                    commit: function(element, terms) {
+                      this.setup(element, terms);
                     }
                 }, ]
             }
@@ -50,43 +64,72 @@ CKEDITOR.dialog.add('definedtermsDialog', function(editor) {
         onShow: function() {
 
             var definedterms = '';
-            var terms = [ 'must', 'should', 'could', 'will not', 'experimental', 'discovery', 'alpha', 'beta', 'live', "release candidate" ];
-
+            var terms = { 'must': '', 'should': '', 'could': '', 'will not':'', 'experimental':'', 'discovery':'', 'beta':'', 'live':'', 'release candidate':'' };
             var selectedText = editor.getSelection().getSelectedText().toString();
-            for (var i = 0; i < terms.length; i++) {
-                var selected = '';
-                if (selectedText.trim().toLowerCase() == terms[i].toLowerCase()) {
-                  selected = " selected";
-                }
-                definedterms += "<option value=\"" + terms[i] + "\""+selected+">" + terms[i] + "</option>";
-            }
-            var dropdownElement = CKEDITOR.dialog.getCurrent().getContentElement('tab-id', 'term').getInputElement();
-            dropdownElement.setHtml(definedterms);
 
+            // assign 'this' to separate var in order to pass it to Ajax request
+            var thisPass = this;
 
-            var element = editor.getSelection().getStartElement();
-
-            if (element) {
-                element = element.getAscendant('span', true);
+            var origin = location.origin; //for localhost-based origin
+            if (origin.search(/cms-[dev,uat,tst,training]/) != -1) {
+              origin = origin.replace("cms-", "");
+            } else if (origin.search("cms.digital.nhs.uk") != -1 || origin.search("cms-nhs.hosting.onehippo.com") != -1){
+              origin = "https://digital.nhs.uk";
             }
 
-            if (!element || element.getName() != 'span') {
-                element = editor.document.createElement('span');
-                this.insertMode = true;
-            } else
-                this.insertMode = false;
+            $.ajax({
+                type: "GET",
+                url: origin+"/restapi/definedterms",
+                dataType: "xml",
+                timeout: 3000,
+                success: function(xml)
+                    {
+                        $(xml).find("definedterm").each(function () {
+                                term = $(this).find("term").text();
+                                colorrgb = $(this).find("colorrgb").text().trim().toLowerCase();
+                                if (! (term in terms) || (term in terms && colorrgb != '')) {
+                                  terms[term] = colorrgb.trim().toLowerCase();
+                                }
+                        });
 
-            this.element = element;
-            if (!this.insertMode)
-                this.setupContent(this.element);
+                        for (var key in terms) {
+                            var selected = '';
+                            if (selectedText.trim().toLowerCase() == key.toLowerCase()) {
+                              selected = " selected";
+                            }
+                            definedterms += "<option value=\"" + key + "\""+selected+">" + key + "</option>";
+                        }
+                        var dropdownElement = CKEDITOR.dialog.getCurrent().getContentElement('tab-id', 'term').getInputElement();
+                        dropdownElement.setHtml(definedterms);
+
+                        var element = editor.getSelection().getStartElement();
+
+                        if (element) {
+                            element = element.getAscendant('span', true);
+                        }
+
+                        if (!element || element.getName() != 'span') {
+                            element = editor.document.createElement('span');
+                            thisPass.insertMode = true;
+                        } else {
+                            thisPass.insertMode = false;
+                        }
+
+                        thisPass.element = element;
+                        thisPass.terms = terms;
+                        if (!thisPass.insertMode)
+                            thisPass.setupContent(thisPass.element, thisPass.terms);
+                    }
+            });
+
         },
 
         onOk: function() {
 
-            var dialog = this;
             var definedterms = this.element;
+            var terms = this.terms;
 
-            this.commitContent(definedterms);
+            this.commitContent(definedterms, terms);
 
             if (this.insertMode)
                 editor.insertElement(definedterms);
