c175f21d12b3441a9c868b644fa8c523e712780a
Damian.Ziobro <damian.ziobro1@nhs.net>
2019-09-23 13:45:30 +0100

    [DW-1025] Implement dynamically added defined terms

---
+++ site/src/main/java/uk/nhs/digital/website/rest/DefinedTermsResource.java
@@ -0,0 +1,29 @@
+package uk.nhs.digital.website.rest;
+
+import org.onehippo.cms7.essentials.components.paging.Pageable;
+import org.onehippo.cms7.essentials.components.rest.BaseRestResource;
+import org.onehippo.cms7.essentials.components.rest.ctx.DefaultRestContext;
+import uk.nhs.digital.website.beans.DefinedTerms;
+
+
+import java.util.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+
+@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_FORM_URLENCODED})
+@Path("/definedterms/")
+public class DefinedTermsResource extends BaseRestResource {
+
+    @GET
+    @Path("/")
+    public Pageable<DefinedTerms> index(@Context HttpServletRequest request) {
+        return findBeans(new DefaultRestContext(this, request), DefinedTerms.class);
+    }
+}
