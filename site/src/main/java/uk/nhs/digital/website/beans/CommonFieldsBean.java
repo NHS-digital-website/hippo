58131e456485140e6e6af5a7b3adfdefa7ebf601
Damian.Ziobro <damian.ziobro1@nhs.net>
2019-11-20 16:14:56 +0000

    update

--- site/src/main/java/uk/nhs/digital/website/beans/CommonFieldsBean.java
+++ site/src/main/java/uk/nhs/digital/website/beans/CommonFieldsBean.java
@@ -32,7 +32,7 @@ public class CommonFieldsBean extends BaseDocument {
 
     protected static int NO_LIMIT = 0;
 
-    @JsonIgnore
+    @JsonProperty
     public String getSeosummaryJson() {
         return getHippoHtmlContent("website:seosummary");
     }
@@ -42,7 +42,7 @@ public class CommonFieldsBean extends BaseDocument {
         return getHippoHtml("website:seosummary");
     }
 
-    @JsonIgnore
+    @JsonProperty
     @HippoEssentialsGenerated(internalName = "website:shortsummary", allowModifications = false)
     public String getShortsummary() {
         return getProperty("website:shortsummary");
