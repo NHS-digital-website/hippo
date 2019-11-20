7fbf73296d34a124b230b857571dbc779d28f3d3
Damian.Ziobro <damian.ziobro1@nhs.net>
2019-11-20 14:10:36 +0000

    [DW-1025] Change API output from XML to JSON and update dialog.js

--- site/src/main/java/uk/nhs/digital/website/beans/DefinedTerm.java
+++ site/src/main/java/uk/nhs/digital/website/beans/DefinedTerm.java
@@ -1,27 +1,24 @@
 package uk.nhs.digital.website.beans;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import org.hippoecm.hst.content.beans.Node;
 import org.hippoecm.hst.content.beans.standard.HippoCompound;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement(name = "definedterm")
-@XmlAccessorType(XmlAccessType.FIELD)
+@JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE)
 @HippoEssentialsGenerated(internalName = "website:definedterm")
 @Node(jcrType = "website:definedterm")
 public class DefinedTerm extends HippoCompound {
 
-    @XmlElement
+    @JsonProperty
     @HippoEssentialsGenerated(internalName = "website:term")
     public String getTerm() {
         return getProperty("website:term");
     }
 
-    @XmlElement
+    @JsonProperty
     @HippoEssentialsGenerated(internalName = "website:colorrgb")
     public String getColorrgb() {
         return getProperty("website:colorrgb");
