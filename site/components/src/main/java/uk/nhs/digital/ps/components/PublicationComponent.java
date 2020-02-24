36ea9d0fe4179b3cef05746cff77e460eff70386
Mohammed Hasan <mohammed.hasan@valtech.com>
2020-02-24 16:31:03 +0000

    [DW-1193] Add new fields to Series + Archive
    Also remove fields from publication + legacy publication doctype
    Add update script to fix taxonomy for existing documents
    Update dev content with taxonomy mixins

--- site/components/src/main/java/uk/nhs/digital/ps/components/PublicationComponent.java
+++ site/components/src/main/java/uk/nhs/digital/ps/components/PublicationComponent.java
@@ -8,7 +8,9 @@ import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
 import org.hippoecm.hst.core.request.HstRequestContext;
 import org.onehippo.cms7.essentials.components.EssentialsContentComponent;
+import uk.nhs.digital.ps.beans.Archive;
 import uk.nhs.digital.ps.beans.Publication;
+import uk.nhs.digital.ps.beans.Series;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -51,7 +53,8 @@ public class PublicationComponent extends EssentialsContentComponent {
             index.add(KEY_FACTS_ID);
         }
 
-        if (isNotBlank(publication.getAdministrativeSources())) {
+        // TODO remove publication check in DW-1285
+        if (parentIsSeriesAndAdminSourcesNotBlank(publication) || parentIsArchiveAndAdminSourcesNotBlank(publication) || isNotBlank(publication.getAdministrativeSources())) {
             index.add(ADMIN_SOURCES_ID);
         }
 
@@ -69,5 +72,13 @@ public class PublicationComponent extends EssentialsContentComponent {
         }
 
         return index;
+    }
+
+    private boolean parentIsSeriesAndAdminSourcesNotBlank(Publication publication) {
+        return publication.getParentDocument() instanceof Series && isNotBlank(((Series) publication.getParentDocument()).getAdministrativeSources());
+    }
+
+    private boolean parentIsArchiveAndAdminSourcesNotBlank(Publication publication) {
+        return publication.getParentDocument() instanceof Archive && isNotBlank(((Archive) publication.getParentDocument()).getAdministrativeSources());
     }
 }
