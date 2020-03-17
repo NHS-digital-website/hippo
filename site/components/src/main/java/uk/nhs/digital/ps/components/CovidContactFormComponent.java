2361b7e6f281a5b5f4e20121b46da1435461ae09
Mohammed Hasan <mohammed.hasan@valtech.com>
2020-03-17 12:17:14 +0000

    [DW-1400] Add dummy component

---
+++ site/components/src/main/java/uk/nhs/digital/ps/components/CovidContactFormComponent.java
@@ -0,0 +1,37 @@
+package uk.nhs.digital.ps.components;
+
+import org.apache.commons.lang3.StringUtils;
+import org.hippoecm.hst.component.support.forms.FormMap;
+import org.hippoecm.hst.component.support.forms.FormUtils;
+import org.hippoecm.hst.core.component.HstComponentException;
+import org.hippoecm.hst.core.component.HstRequest;
+import org.hippoecm.hst.core.component.HstResponse;
+import org.onehippo.cms7.essentials.components.CommonComponent;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CovidContactFormComponent extends CommonComponent {
+
+    private static final String POSTCODE = "postcode";
+    private static final String AGE = "age";
+    private static final String NAME = "name";
+    private static final String CONTACT_INFO = "contact";
+    private static final List<String> FORM_FIELDS = Arrays
+        .asList(POSTCODE, AGE, NAME, CONTACT_INFO);
+
+    @Override
+    public void doAction(HstRequest request, HstResponse response)
+        throws HstComponentException {
+
+        FormMap map = new FormMap(request, FORM_FIELDS);
+
+        String postcode = map.getField(POSTCODE).getValue();
+
+        if (StringUtils.isBlank(postcode)) {
+            FormUtils.persistFormMap(request, response, map, null);
+        }
+
+        // POST the values
+    }
+}
