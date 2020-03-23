23275400b29240edd96ac8952740564d76ac0c8c
Mohammed Hasan <mohammed.hasan@valtech.com>
2020-03-23 13:14:34 +0000

    [DW-1225] Create component + sitemap

---
+++ repository-data/webfiles/src/main/resources/site/freemarker/intranet/task.ftl
@@ -0,0 +1,9 @@
+<#ftl output_format="HTML">
+<#include "../include/imports.ftl">
+
+<article class="article article--intranet-home">
+    <div class="grid-wrapper grid-wrapper--article">
+        <h2>${document.title}</h2>
+        <h2>${document.reviewDate}</h2>
+    </div>
+</article>
---
+++ site/components/src/main/java/uk/nhs/digital/intranet/components/TaskComponent.java
@@ -0,0 +1,14 @@
+package uk.nhs.digital.intranet.components;
+
+import org.hippoecm.hst.core.component.HstRequest;
+import org.hippoecm.hst.core.component.HstResponse;
+import org.onehippo.cms7.essentials.components.EssentialsContentComponent;
+
+public class TaskComponent extends EssentialsContentComponent {
+
+    @Override
+    public void doBeforeRender(HstRequest request,
+        HstResponse response) {
+        super.doBeforeRender(request, response);
+    }
+}
