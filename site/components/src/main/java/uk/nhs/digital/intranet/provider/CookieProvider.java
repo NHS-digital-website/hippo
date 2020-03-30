c07d461bcb89cd2613c21059c9baf6cfd42fee1a
Alex <alexandru.mitcu@valtech.com>
2020-03-30 11:27:20 +0100

    [DW-1442] Added access token valve which reads the access token cookie

--- site/components/src/main/java/uk/nhs/digital/intranet/provider/CookieProvider.java
+++ site/components/src/main/java/uk/nhs/digital/intranet/provider/CookieProvider.java
@@ -1,31 +1,21 @@
 package uk.nhs.digital.intranet.provider;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import uk.nhs.digital.intranet.model.AccessToken;
 import uk.nhs.digital.intranet.utils.AccessTokenEncoder;
-
-import java.io.IOException;
 
 import javax.servlet.http.Cookie;
 
 public class CookieProvider {
 
-    private static final String ACCESS_TOKEN_COOKIE_NAME = "MGAT";
-    private static final Logger LOGGER = LoggerFactory.getLogger(CookieProvider.class);
+    public static final String ACCESS_TOKEN_COOKIE_NAME = "__MGACCESSTOKEN";
     private static final int TTL = 30 * 24 * 3600;
+    private static final String COOKIE_PATH = "/site/intranet";
 
     public static Cookie getAccessTokenCookie(final AccessToken accessToken) {
-        final String encodedAccessToken;
-        try {
-            encodedAccessToken = AccessTokenEncoder.encode(accessToken);
-            final Cookie accessTokenCookie = new Cookie(ACCESS_TOKEN_COOKIE_NAME, encodedAccessToken);
-            accessTokenCookie.setPath("/intranet");
-            accessTokenCookie.setMaxAge(TTL);
-            return accessTokenCookie;
-        } catch (final IOException e) {
-            LOGGER.error("Could not encode access token.", e);
-            return null;
-        }
+        final String encodedAccessToken = AccessTokenEncoder.encode(accessToken);
+        final Cookie accessTokenCookie = new Cookie(ACCESS_TOKEN_COOKIE_NAME, encodedAccessToken);
+        accessTokenCookie.setPath(COOKIE_PATH);
+        accessTokenCookie.setMaxAge(TTL);
+        return accessTokenCookie;
     }
 }
---
+++ site/components/src/main/java/uk/nhs/digital/intranet/valves/AccessTokenValve.java
@@ -0,0 +1,48 @@
+package uk.nhs.digital.intranet.valves;
+
+import org.hippoecm.hst.container.valves.AbstractOrderableValve;
+import org.hippoecm.hst.core.container.ContainerException;
+import org.hippoecm.hst.core.container.ValveContext;
+import org.hippoecm.hst.core.request.HstRequestContext;
+import uk.nhs.digital.intranet.model.AccessToken;
+import uk.nhs.digital.intranet.provider.CookieProvider;
+import uk.nhs.digital.intranet.utils.AccessTokenEncoder;
+
+import java.util.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+public class AccessTokenValve extends AbstractOrderableValve {
+
+    private static final String ACCESS_TOKEN_PROPERTY = "accessToken";
+
+    @Override
+    public void invoke(ValveContext context) throws ContainerException {
+        final HstRequestContext requestContext = context.getRequestContext();
+        final HttpServletRequest request = requestContext.getServletRequest();
+        if (request.getAttribute(ACCESS_TOKEN_PROPERTY) == null) {
+            final Optional<Cookie> cookieOptional = getAccessTokenCookie(request);
+            cookieOptional.ifPresent(cookie -> {
+                final AccessToken accessToken = AccessTokenEncoder.decode(cookie.getValue());
+                if (accessToken != null) {
+                    request.setAttribute(ACCESS_TOKEN_PROPERTY, accessToken.getToken());
+                }
+            });
+        }
+        context.invokeNext();
+    }
+
+    private Optional<Cookie> getAccessTokenCookie(final HttpServletRequest request) {
+        return getCookies(request)
+            .stream()
+            .filter(Objects::nonNull)
+            .filter(cookie -> CookieProvider.ACCESS_TOKEN_COOKIE_NAME.equals(cookie.getName()))
+            .findAny();
+    }
+
+    private List<Cookie> getCookies(final HttpServletRequest request) {
+        final Cookie[] cookies = request.getCookies();
+        return cookies != null ? Arrays.asList(cookies) : Collections.emptyList();
+    }
+}
