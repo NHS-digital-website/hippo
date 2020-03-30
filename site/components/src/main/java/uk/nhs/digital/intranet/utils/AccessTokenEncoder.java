d9b0cc4e25f95a7ad37d411e61e0ac44381cdad0
Alex <alexandru.mitcu@valtech.com>
2020-03-30 11:27:20 +0100

    [DW-1442] Added access token valve which reads the access token cookie

--- site/components/src/main/java/uk/nhs/digital/intranet/utils/AccessTokenEncoder.java
+++ site/components/src/main/java/uk/nhs/digital/intranet/utils/AccessTokenEncoder.java
@@ -1,5 +1,7 @@
 package uk.nhs.digital.intranet.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.nhs.digital.intranet.model.AccessToken;
 
 import java.io.*;
@@ -7,20 +9,32 @@ import java.util.Base64;
 
 public class AccessTokenEncoder {
 
-    public static String encode(final AccessToken accessToken) throws IOException {
-        final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        final ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
-        objectOutputStream.writeObject(accessToken);
-        objectOutputStream.close();
-        return Base64.getEncoder().encodeToString(byteArrayOutputStream.toByteArray());
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccessTokenEncoder.class);
+
+    public static String encode(final AccessToken accessToken) {
+        try {
+            final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            final ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+            objectOutputStream.writeObject(accessToken);
+            objectOutputStream.close();
+            return Base64.getEncoder().encodeToString(byteArrayOutputStream.toByteArray());
+        } catch (final IOException e) {
+            LOGGER.error("Could not encode access token.", e);
+            return null;
+        }
     }
 
-    public static AccessToken decode(final String encodedString) throws IOException, ClassNotFoundException {
-        final byte[] bytes = Base64.getDecoder().decode(encodedString);
-        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
-        final ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
-        final AccessToken accessToken = (AccessToken) objectInputStream.readObject();
-        objectInputStream.close();
-        return accessToken;
+    public static AccessToken decode(final String encodedString) {
+        try {
+            final byte[] bytes = Base64.getDecoder().decode(encodedString);
+            final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+            final ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+            final AccessToken accessToken = (AccessToken) objectInputStream.readObject();
+            objectInputStream.close();
+            return accessToken;
+        } catch (final Exception e) {
+            LOGGER.error("Could not decode access token.", e);
+            return null;
+        }
     }
 }
