11e5d5d639eac76dc39a43533c6cc59fa3143de0
Alex <alexandru.mitcu@valtech.com>
2020-03-30 15:58:24 +0100

    [DW-1442] Transformed remaining classes into Spring beans

--- site/components/src/main/java/uk/nhs/digital/intranet/provider/CookieProvider.java
+++ site/components/src/main/java/uk/nhs/digital/intranet/provider/CookieProvider.java
@@ -11,8 +11,14 @@ public class CookieProvider {
     private static final int TTL = 30 * 24 * 3600;
     private static final String COOKIE_PATH = "/site/intranet";
 
+    private final AccessTokenEncoder encoder;
+
+    public CookieProvider(final AccessTokenEncoder encoder) {
+        this.encoder = encoder;
+    }
+
     public Cookie getAccessTokenCookie(final AccessToken accessToken) {
-        final String encodedAccessToken = AccessTokenEncoder.encode(accessToken);
+        final String encodedAccessToken = encoder.encode(accessToken);
         final Cookie accessTokenCookie = new Cookie(ACCESS_TOKEN_COOKIE_NAME, encodedAccessToken);
         accessTokenCookie.setPath(COOKIE_PATH);
         accessTokenCookie.setMaxAge(TTL);
