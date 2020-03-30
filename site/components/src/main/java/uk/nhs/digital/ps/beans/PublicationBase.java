8867143b7a1cba90dd05f8e3a8621979c1314006
Srinivas Begari <srinivas.begari@valtech.com>
2020-03-30 15:38:52 +0100

    [DW-1197] DW-1197/98-refactor publication/legacy publication keyfacts

--- site/components/src/main/java/uk/nhs/digital/ps/beans/PublicationBase.java
+++ site/components/src/main/java/uk/nhs/digital/ps/beans/PublicationBase.java
@@ -278,6 +278,7 @@ public abstract class PublicationBase extends BaseDocument {
         String RELATED_LINKS = "publicationsystem:RelatedLinks";
         String RESOURCE_LINKS = "publicationsystem:ResourceLinks";
         String ATTACHMENTS_V3 = "publicationsystem:Attachments-v3";
+        String HIGHLIGHTS = "publicationsystem:highlights";
 
         String PARENT_BEAN = "PARENT_BEAN";
         String PARENT_SERIES = "PARENT_SERIES";
