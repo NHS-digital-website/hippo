0d425ef158283d3938583f056a8d33cca2224eaa
Srinivas Begari <srinivas.begari@valtech.com>
2020-03-31 12:02:18 +0100

    [DW-1197] DW-1197/98 adding highlights model attribute to publication

--- site/components/src/main/java/uk/nhs/digital/ps/beans/Publication.java
+++ site/components/src/main/java/uk/nhs/digital/ps/beans/Publication.java
@@ -1,6 +1,5 @@
 package uk.nhs.digital.ps.beans;
 
-import org.apache.commons.collections.CollectionUtils;
 import org.hippoecm.hst.content.beans.Node;
 import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.hippoecm.hst.content.beans.standard.HippoHtml;
@@ -11,7 +10,6 @@ import uk.nhs.digital.ps.beans.structuredText.StructuredText;
 import uk.nhs.digital.website.beans.Infographic;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @HippoEssentialsGenerated(internalName = "publicationsystem:publication")
@@ -58,13 +56,7 @@ public class Publication extends PublicationBase implements Paginated {
 
     @HippoEssentialsGenerated(internalName = PropertyKeys.HIGHLIGHTS)
     public List<HippoBean> getHighlights() {
-        if (CollectionUtils.isEmpty(getChildBeansIfPermitted(PropertyKeys.HIGHLIGHTS,
-            HippoBean.class))) {
-            return Collections.EMPTY_LIST;
-        } else {
-            return getChildBeansIfPermitted(PropertyKeys.HIGHLIGHTS,
-                HippoBean.class);
-        }
+       return getChildBeansIfPermitted(PropertyKeys.HIGHLIGHTS, HippoBean.class);
     }
 
     public List<PublicationPage> getPages() {
