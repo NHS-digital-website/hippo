02bf014d4d6e454e3d4de9d835dcf7422cb377e1
GeoffHayward <geoffrey.hayward@nhs.net>
2020-05-21 10:48:37 +0100

    [DW-000] Adds on/off switch to banner

--- site/components/src/main/java/uk/nhs/digital/common/components/CoronavirusBanner.java
+++ site/components/src/main/java/uk/nhs/digital/common/components/CoronavirusBanner.java
@@ -2,10 +2,12 @@ package uk.nhs.digital.common.components;
 
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
+import org.hippoecm.hst.resourcebundle.ResourceBundleUtils;
 import org.onehippo.cms7.essentials.components.EssentialsContentComponent;
 
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.ResourceBundle;
 import javax.servlet.http.Cookie;
 
 public class CoronavirusBanner extends EssentialsContentComponent {
@@ -16,7 +18,9 @@ public class CoronavirusBanner extends EssentialsContentComponent {
     }
 
     private boolean shouldShowBanner(HstRequest request) {
-        return !userHasHiddenBanner(request) && !isCoronavirusDirectory(request);
+        return bannerIsSwitchedOn(request)
+            && !userHasHiddenBanner(request)
+            && !isCoronavirusDirectory(request);
     }
 
     private boolean userHasHiddenBanner(HstRequest request) {
@@ -26,5 +30,14 @@ public class CoronavirusBanner extends EssentialsContentComponent {
 
     private boolean isCoronavirusDirectory(HstRequest request) {
         return request.getRequestContext().getBaseURL().getPathInfo().toLowerCase().startsWith("/coronavirus");
+    }
+
+    private boolean bannerIsSwitchedOn(HstRequest request) {
+        ResourceBundle bundle = ResourceBundleUtils.getBundle("coronavirus.banner", request.getLocale());
+        if (bundle.containsKey("show-banner")) {
+            String shouldShow = bundle.getString("show-banner");
+            return Boolean.valueOf(shouldShow);
+        }
+        return false;
     }
 }
