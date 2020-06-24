5c45063c8a0a38b3e843983efb08b06617d36a8f
Jodi Warren <jodi@jodiwarren.com>
2020-06-24 17:45:33 +0100

    [DW-000] Fix chart accessibility
    This integrate missed changes from DW-1500 that weren't
    fully integrated into the FE changes in DW-1494

--- repository-data/webfiles/src/main/resources/site/src/js/highcharts/highchart-plugins.js
+++ repository-data/webfiles/src/main/resources/site/src/js/highcharts/highchart-plugins.js
@@ -1,9 +1,11 @@
 import Data from 'highcharts/modules/data';
+import Accessibility from 'highcharts/modules/accessibility';
 import Exporting from 'highcharts/modules/exporting';
 import OfflineExporting from 'highcharts/modules/offline-exporting';
 
 export function highchartPlugins(highcharts) {
     Data(highcharts);
+    Accessibility(highcharts);
     Exporting(highcharts);
     OfflineExporting(highcharts);
 }
