f665c180ad5764581e9748034a38c8ac62e66346
Jodi Warren <jodi@jodiwarren.com>
2020-07-07 15:40:02 +0100

    [DW-1456] Enable statistic countup on numbers with decimal places
    - Fix validation to allow statistic numbers with decimal points
    - Respect prefer-reduced-motion

--- repository-data/webfiles/src/main/resources/site/src/js/statistics/statistics-countup.js
+++ repository-data/webfiles/src/main/resources/site/src/js/statistics/statistics-countup.js
@@ -20,10 +20,36 @@ function checkIntersectionObserver() {
     return false
 }
 
+function countDecimalPlaces(num) {
+    if (Number.isNaN(Number(num))) {
+        return 0;
+    }
+
+    if(num.indexOf('.') === -1) {
+        return 0;
+    }
+
+    const lastItem = String(num).split('.').slice(-1)[0]
+
+    return Number(lastItem.length)
+
+}
+
 function handleScrollIntoView(entries, observer) {
     entries.forEach(function (entry) {
         if (entry.intersectionRatio >= 1) {
-            const thisCount = new CountUp(entry.target, entry.target.dataset.targetNum);
+            const {targetNum} = entry.target.dataset;
+
+            const options = {};
+
+            const decimalPlaces = countDecimalPlaces(targetNum);
+
+            if (decimalPlaces) {
+                options.decimalPlaces = decimalPlaces;
+            }
+
+            const thisCount = new CountUp(entry.target, Number(entry.target.dataset.targetNum), options);
+
             if (!thisCount.error) {
                 thisCount.start(function () {
                     observer.unobserve(entry.target);
@@ -38,6 +64,17 @@ function handleScrollIntoView(entries, observer) {
 export function initCountup() {
     if (!checkIntersectionObserver()) {
         return;
+    }
+
+    // No animation for reduced motion users
+    if (window.matchMedia('(prefers-reduced-motion)').matches) {
+        return;
+    }
+
+    // Add Number.isNaN polyfill
+    Number.isNaN = Number.isNaN || function isNaN(input) {
+        // eslint-disable-next-line no-self-compare
+        return typeof input === 'number' && input !== input;
     }
 
     const countUpItems = [].slice.call(document.querySelectorAll('.statistics-section--animated .statistic--single-number .statistic__number'));
