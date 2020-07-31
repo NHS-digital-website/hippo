72bbd24fd72447f4669f55b8d4df0facb735a205
Alex <alexandru.mitcu@valtech.com>
2020-07-31 14:45:20 +0100

    [DW-1521] Added back-end configuration

---
+++ site/components/src/main/java/uk/nhs/digital/common/forms/ApiAndAfterProcessEformComponent.java
@@ -0,0 +1,42 @@
+package uk.nhs.digital.common.forms;
+
+import com.onehippo.cms7.eforms.hst.behaviors.AfterProcessBehavior;
+import com.onehippo.cms7.eforms.hst.components.EformComponent;
+import com.onehippo.cms7.eforms.hst.model.ErrorMessage;
+import com.onehippo.cms7.eforms.hst.model.Form;
+import org.hippoecm.hst.component.support.forms.FormMap;
+import org.hippoecm.hst.core.component.HstRequest;
+import org.hippoecm.hst.core.component.HstResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class ApiAndAfterProcessEformComponent extends EformComponent {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApiAndAfterProcessEformComponent.class);
+
+    @Override
+    public boolean onValidationSuccess(HstRequest request, HstResponse response, Form form, FormMap map) {
+        return true;
+    }
+
+    @Override
+    public void onValidationError(HstRequest request, HstResponse response, Form form, FormMap map,
+                                  Map<String, ErrorMessage> errors) {
+        LOGGER.info("Form contains errors");
+        // Called on second pass (RENDER_PHASE)
+        if ("RENDER_PHASE".equals(request.getLifecyclePhase()) && map.getFormMap().get("eforms_process_done") == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
+    @Override
+    protected void addConfiguredBehaviors(HstRequest request) {
+        super.addConfiguredBehaviors(request);
+        addBehavior(new AfterProcessBehavior());
+        addBehavior(new ReCaptchaValidationPlugin());
+    }
+}
