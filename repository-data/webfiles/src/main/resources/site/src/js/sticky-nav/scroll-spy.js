697eb85220fe41718ae389894acaad570702bf3e
Lesley Kelly <lesley.kelly2@nhs.net>
2020-08-20 09:53:31 +0100

    Conflict with scroll and click fix

--- repository-data/webfiles/src/main/resources/site/src/js/sticky-nav/scroll-spy.js
+++ repository-data/webfiles/src/main/resources/site/src/js/sticky-nav/scroll-spy.js
@@ -7,7 +7,7 @@ export function initScrollSpy() {
 
 	// lets make sure the page loads as the offsettop might not be accurate
 	window.addEventListener("DOMContentLoaded", function() {
-		var section = document.querySelectorAll(".article-section-nav, .article-section, .article-section-separator, .article-section--summary-separator, .sticky-nav-spy-item");
+		var section = document.querySelectorAll(".article-section, .article-section-separator, .article-section--summary-separator, .sticky-nav-spy-item");
 		var sections = {};
 		var i = 0;
 
@@ -15,23 +15,34 @@ export function initScrollSpy() {
 			sections[e.id] = e.offsetTop;
 		});
 
-		document.addEventListener("scroll", function() {
-				var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+        document.addEventListener("click", function() {
+            var navLinks = document.querySelectorAll(".article-section-nav__list a");
+            var j;
 
-				for (i in sections) {
-					if (sections[i] <= scrollPosition) {
-						if (document.querySelector('.active') !== null) {
-							document.querySelector('.active').setAttribute('class', ' ');
-						}
+            for (j = 0; j < navLinks.length; j++) {
+                if (navLinks[j] == event.target) {
+                    navLinks[j].classList.add("active");
+                }
+                else{
+                navLinks[j].classList.remove("active");
+                }
+            }
+        });
 
-						if (document.querySelector('a[href\*=' + i + ']') !== null) {
-							document.querySelector('a[href\*=' + i + ']').setAttribute('class', 'active');
-						//if (document.querySelector('a[href="#${i}"]') !== null) {
-						//	document.querySelector('a[href="#${i}"]').setAttribute('class', 'active');
-						}
+		document.addEventListener("wheel", function() {
+			var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+			for (i in sections) {
+				if (sections[i] <= scrollPosition) {
+					if (document.querySelector('.active') !== null) {
+						document.querySelector('.active').setAttribute('class', ' ');
+					}
+
+					if (document.querySelector('a[href\*=' + i + ']') !== null) {
+						document.querySelector('a[href\*=' + i + ']').setAttribute('class', 'active');
 					}
 				}
-			});
-		}
-	);
+			}
+		});
+	});
 }
--- repository-data/webfiles/src/main/resources/site/src/js/sticky-nav/sticky-nav.js
+++ repository-data/webfiles/src/main/resources/site/src/js/sticky-nav/sticky-nav.js
@@ -1,41 +1,45 @@
 import "css-element-queries/src/ResizeSensor";
 import StickySidebar from "sticky-sidebar";
 
-import {initScrollSpy} from "./scroll-spy";
+import {
+	initScrollSpy
+}
+from "./scroll-spy";
 
 const stickyNav = document.querySelector('#sticky-nav');
 const chapterNav = document.querySelector('.chapter-pagination-wrapper');
 
 function getStickyOffset() {
-    const CHAPTER_NAV_SPACING = 24;
-    const SIDEBAR_SPACING = 20;
+	const CHAPTER_NAV_SPACING = 24;
+	const SIDEBAR_SPACING = 20;
 
-    return chapterNav ? (chapterNav.offsetHeight + (CHAPTER_NAV_SPACING + SIDEBAR_SPACING)) : SIDEBAR_SPACING;
+	return chapterNav ? (chapterNav.offsetHeight + (CHAPTER_NAV_SPACING + SIDEBAR_SPACING)) : SIDEBAR_SPACING;
 }
 
 export function initStickyNav() {
-    if (stickyNav) {
-        initScrollSpy();
+	if (stickyNav) {
+		initScrollSpy();
 
-        const sidebar = new StickySidebar('#sticky-nav', {
-            topSpacing: getStickyOffset(),
-            bottomSpacing: 40,
-            resizeSensor: true,
-            containerSelector: '.grid-row'
-        });
+		const sidebar = new StickySidebar('#sticky-nav', {
+			topSpacing: getStickyOffset(),
+			bottomSpacing: 40,
+			resizeSensor: true,
+			containerSelector: '.grid-row'
+		});
 
-        $(window).on('load resize', function () {
-            if (window.matchMedia('(max-width: 924px)').matches) {
-                sidebar.destroy();
-            } else {
-                sidebar.initialize();
-            }
-        });
+		$(window).on('load resize', function() {
+			if (window.matchMedia('(max-width: 924px)').matches) {
+				sidebar.destroy();
+			} else {
+				sidebar.initialize();
+			}
+		});
 
-        $('.article-section-nav__list a').click(function () {
-            const match = jQuery(this).attr('href').match(/#\S+/);
-            const url = match ? window.location.pathname + match[0] : window.location.pathname;
-            logGoogleAnalyticsEvent('Link click', 'Section Nav', url);
-        });
-    }
+
+		$('.article-section-nav__list a').click(function() {
+			const match = jQuery(this).attr('href').match(/#\S+/);
+			const url = match ? window.location.pathname + match[0] : window.location.pathname;
+			logGoogleAnalyticsEvent('Link click', 'Section Nav', url);
+		});
+	}
 }
