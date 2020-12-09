6a738a2f3b9c2ddcd4b8394e5e3f368b13afa0d0
Joseph Cyrus <Joseph.Cyrus@valtech.com>
2020-12-09 17:22:47 +0000

    [DW-1861] Apply updates to graphic block doctype

--- site/components/src/main/java/uk/nhs/digital/website/beans/GraphicBlock.java
+++ site/components/src/main/java/uk/nhs/digital/website/beans/GraphicBlock.java
@@ -1,19 +1,34 @@
 package uk.nhs.digital.website.beans;
 
 import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.hippoecm.hst.content.beans.standard.HippoGalleryImageSet;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+
+import java.util.List;
 
 @HippoEssentialsGenerated(internalName = "website:graphicblock")
 @Node(jcrType = "website:graphicblock")
 public class GraphicBlock extends CommonFieldsBean {
 
-    @HippoEssentialsGenerated(internalName = "website:title")
-    public String getTitle() {
-        return getSingleProperty("website:title");
+    @HippoEssentialsGenerated(internalName = "website:modules")
+    public List<HippoBean> getModules() {
+        return getChildBeansByName("website:modules");
     }
 
-    @HippoEssentialsGenerated(internalName = "website:infographic")
-    public Infographic getInfographics() {
-        return getBean("website:infographic", Infographic.class);
+    @HippoEssentialsGenerated(internalName = "website:image")
+    public HippoGalleryImageSet getImage() {
+        return getLinkedBean("website:image", HippoGalleryImageSet.class);
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:introductionText")
+    public HippoHtml getIntroductionText() {
+        return getHippoHtml("website:introductionText");
+    }
+
+    @HippoEssentialsGenerated(internalName = "website:items")
+    public List<?> getItems() {
+        return getChildBeansByName("website:items");
     }
 }
