21fcdcbd386b5a221d02788a64ff8ce572aa819e
Rafał Kociński <rafal.kocinski@infinityworks.com>
2021-01-19 16:31:21 +0000

    [ADZ-343] Balance specification headings

--- cms/src/test/java/uk/nhs/digital/apispecs/handlebars/BodyRenderingTest.java
+++ cms/src/test/java/uk/nhs/digital/apispecs/handlebars/BodyRenderingTest.java
@@ -1,7 +1,7 @@
 package uk.nhs.digital.apispecs.handlebars;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertThat;
 import static uk.nhs.digital.test.util.FileUtils.contentOfFileFromClasspath;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
