1692ba7242731f2116a47cae5e708c2ca53af95a
Rafał Kociński <rafal.kocinski@infinityworks.com>
2021-03-18 08:46:02 +0000

    [ADZ-987] Defer API Spec batch jobs scheduling
    The scheduling of API Catalogue-related batch
    jobs now takes place with a delay after the
    system starts. The delay is configurable as system
    property `devzone.apispec.sync.schedule-delay-duration`;
    default value of 45 minutes is applied if the
    property is not configured. A background thread is
    spawned on system start which stays dormant for
    the duration of the delay after which it wakes up,
    schedules the jobs, terminates and disappears.
    
    The change also fixes the `Error while shutting down
    daemon module org.onehippo.cms7.services.HippoServiceException:
    A service of type uk.nhs.digital.externalstorage.s3.PooledS3Connector
    is already registered.`, where the service was attempted to
    be registered, rather than unregistered on module/system shutdown,
    by mistake.

--- cms/src/main/java/uk/nhs/digital/apispecs/module/ApiSpecSyncFromApigeeModule.java
+++ cms/src/main/java/uk/nhs/digital/apispecs/module/ApiSpecSyncFromApigeeModule.java
@@ -1,7 +1,11 @@
 package uk.nhs.digital.apispecs.module;
 
+import static java.lang.String.format;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import org.apache.commons.lang3.StringUtils;
 import org.onehippo.cms7.services.HippoServiceRegistry;
-import org.onehippo.repository.modules.AbstractReconfigurableDaemonModule;
+import org.onehippo.repository.modules.DaemonModule;
 import org.onehippo.repository.modules.RequiresService;
 import org.onehippo.repository.scheduling.RepositoryJob;
 import org.onehippo.repository.scheduling.RepositoryJobCronTrigger;
@@ -9,80 +13,108 @@ import org.onehippo.repository.scheduling.RepositoryJobInfo;
 import org.onehippo.repository.scheduling.RepositoryScheduler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import uk.nhs.digital.JcrNodeUtils;
+import uk.nhs.digital.apispecs.exception.ApiCatalogueJobException;
 import uk.nhs.digital.apispecs.jobs.ApiSpecRerenderJob;
 import uk.nhs.digital.apispecs.jobs.ApiSpecSyncFromApigeeJob;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Optional;
-import java.util.stream.Stream;
-import javax.jcr.Node;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 
 @RequiresService(types = { RepositoryScheduler.class } )
-public class ApiSpecSyncFromApigeeModule extends AbstractReconfigurableDaemonModule {
+public class ApiSpecSyncFromApigeeModule implements DaemonModule {
 
     private static final Logger log = LoggerFactory.getLogger(ApiSpecSyncFromApigeeModule.class);
 
-    private final Object configurationLock = new Object();
+    // See JavaDoc of java.time.Duration.parse for format.
+    private static final String SCHEDULING_DELAY_DEFAULT_VALUE = "PT45M";
+
+    private static final String SCHEDULING_DELAY_PROPERTY_NAME = "devzone.apispec.sync.schedule-delay-duration";
 
     private static final String JOB_GROUP = "devzone";
     private static final String JOB_TRIGGER_NAME = "cronTrigger";
 
-    @Override protected void doConfigure(final Node moduleConfigNode) {
+    @Override public void initialize(final Session session) throws RepositoryException {
 
-        // This method is called both on system startup AND on update to the node through the admin console.
-        //
-        // Given that we need to update the job on both events, we create and configure the
-        // job here rather than in doInitialize which is only being called on system startup.
+        try {
+            final Duration schedulingDelay = configurableSchedulingDelay();
 
-        synchronized (configurationLock) {
-            for (final ScheduledJobs job: ScheduledJobs.values()) {
-                try {
-                    log.info("Configuring job {}/{} for updating API specifications.", JOB_GROUP, job.jobName); // the only one called on the config node change
+            scheduleJobsWithDelay(schedulingDelay);
 
-                    deactivatePreviousJobInstanceIfExist(job.jobName);
-
-                    if (jobShouldBeEnabled(moduleConfigNode, job)) {
-
-                        scheduleNewJob(moduleConfigNode, job);
-
-                    } else {
-                        log.warn("Job {}/{} is not configured to be enabled, therefore it will not be activated.", JOB_GROUP, job.jobName);
-                    }
-
-                } catch (final Exception e) {
-                    throw new RuntimeException(String.format("Failed to configure job %s/%s", JOB_GROUP, job.jobName), e);
-                }
-            }
+        } catch (final Exception e) {
+            log.error("Failed to schedule API Catalogue related jobs.", e);
         }
     }
 
-    @Override protected void doInitialize(final Session session) {
-        // no-op: all the work is done in doConfigure - see the comment in that method
+    private void scheduleJobsWithDelay(final Duration schedulingDelay) {
+
+        final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+
+        Arrays.stream(ScheduledJobs.values()).forEach(job -> {
+            log.info("Deferring scheduling of job " + job.jobName + " by " + schedulingDelay);
+
+            scheduledExecutorService.schedule(() -> scheduleInJcr(job), schedulingDelay.getSeconds(), SECONDS);
+        });
+
+        scheduledExecutorService.shutdown();
     }
 
-    @Override protected void doShutdown() {
+    private void scheduleInJcr(final ScheduledJobs job) {
+
+        try {
+            log.info("Configuring job {}/{} for updating API specifications.", JOB_GROUP, job.jobName);
+
+            deactivatePreviousJobInstanceIfExists(job.jobName);
+
+            if (shouldBeEnabled(job)) {
+
+                schedule(job);
+
+            } else {
+                log.warn("Job {}/{} is not configured to be enabled, therefore it will not be activated.", JOB_GROUP, job.jobName);
+            }
+
+        } catch (final Exception e) {
+            throw new ApiCatalogueJobException(format("Failed to schedule job %s/%s", JOB_GROUP, job.jobName), e);
+        }
+    }
+
+    @Override public void shutdown() {
         Arrays.stream(ScheduledJobs.values())
             .map(x -> x.jobName)
             .forEach(jobName -> {
                 try {
-                    deactivatePreviousJobInstanceIfExist(jobName);
+                    deactivatePreviousJobInstanceIfExists(jobName);
                 } catch (final Exception e) {
-                    throw new RuntimeException(String.format("Failed to cleanly shut down job %s/%s", JOB_GROUP, jobName), e);
+                    log.error(format("Failed to cleanly shut down job %s/%s", JOB_GROUP, jobName), e);
                 }
             });
     }
 
-    private void scheduleNewJob(
-        final Node moduleConfigNode,
-        final ScheduledJobs job
-    ) throws RepositoryException {
+    private Duration configurableSchedulingDelay() {
+
+        final String schedulingDelayRaw = Optional.ofNullable(System.getProperty(SCHEDULING_DELAY_PROPERTY_NAME))
+            .filter(StringUtils::isNotBlank)
+            .map(String::trim)
+            .orElse(SCHEDULING_DELAY_DEFAULT_VALUE);
+
+        try {
+            return Duration.parse(schedulingDelayRaw);
+
+        } catch (final Exception e) {
+            throw new IllegalArgumentException("Scheduling duration is not in a valid ISO-8601 format: " + schedulingDelayRaw, e);
+        }
+    }
+
+    private void schedule(final ScheduledJobs job) throws RepositoryException {
 
         final RepositoryJobInfo jobInfo = new RepositoryJobInfo(job.jobName, JOB_GROUP, job.jobClass);
 
-        final String cronExpression = cronExpression(moduleConfigNode, job);
+        final String cronExpression = cronExpression(job);
 
         final RepositoryJobCronTrigger trigger = new RepositoryJobCronTrigger(JOB_TRIGGER_NAME, cronExpression);
 
@@ -91,7 +123,7 @@ public class ApiSpecSyncFromApigeeModule extends AbstractReconfigurableDaemonMod
         log.info("Job {}/{} has been scheduled with cron expression: {}", JOB_GROUP, job.jobName, cronExpression);
     }
 
-    private void deactivatePreviousJobInstanceIfExist(final String jobName) throws RepositoryException {
+    private void deactivatePreviousJobInstanceIfExists(final String jobName) throws RepositoryException {
         if (scheduler().checkExists(jobName, JOB_GROUP)) {
             scheduler().deleteJob(jobName, JOB_GROUP);
             log.info("Job {}/{} has been deactivated.", JOB_GROUP, jobName);
@@ -102,25 +134,18 @@ public class ApiSpecSyncFromApigeeModule extends AbstractReconfigurableDaemonMod
         return HippoServiceRegistry.getService(RepositoryScheduler.class);
     }
 
-    private boolean jobShouldBeEnabled(final Node moduleConfigNode, final ScheduledJobs job) {
-
-        final Optional<String> cronExprFromJcr = JcrNodeUtils.getStringPropertyQuietly(moduleConfigNode, job.jcrPropertyName);
+    private boolean shouldBeEnabled(final ScheduledJobs job) {
 
         final Optional<String> cronExprFromSystemProperty = Optional.ofNullable(System.getProperty(job.systemPropertyName));
 
-        return Stream.of(cronExprFromJcr, cronExprFromSystemProperty).anyMatch(Optional::isPresent);
+        return cronExprFromSystemProperty.isPresent();
     }
 
-    private String cronExpression(final Node moduleConfigNode, final ScheduledJobs job) {
-
-        final Optional<String> cronExprFromJcr = JcrNodeUtils.getStringPropertyQuietly(moduleConfigNode, job.jcrPropertyName);
+    private String cronExpression(final ScheduledJobs job) {
 
         final Optional<String> cronExprFromSystemProperty = Optional.ofNullable(System.getProperty(job.systemPropertyName));
 
-        return Stream.of(cronExprFromJcr, cronExprFromSystemProperty)
-            .filter(Optional::isPresent)
-            .map(Optional::get)
-            .findFirst()
+        return cronExprFromSystemProperty
             .orElseThrow(() -> new RuntimeException("Cron expression has not been configured."));
     }
 
@@ -129,26 +154,22 @@ public class ApiSpecSyncFromApigeeModule extends AbstractReconfigurableDaemonMod
         DAILY(
             "apiSpecSyncFromApigee",
             ApiSpecSyncFromApigeeJob.class,
-            "devzone.apispec.sync.daily-cron-expression",
-            "cronExpression"
+            "devzone.apispec.sync.daily-cron-expression"
         ),
         NIGHTLY(
             "apiSpecRerender",
             ApiSpecRerenderJob.class,
-            "devzone.apispec.sync.nightly-cron-expression",
-            "cronExpression"
+            "devzone.apispec.sync.nightly-cron-expression"
         );
 
         private final String jobName;
         private final Class<? extends RepositoryJob> jobClass;
         private final String systemPropertyName;
-        private final String jcrPropertyName;
 
-        ScheduledJobs(final String jobName, final Class<? extends RepositoryJob> jobClass, final String systemPropertyName, final String jcrPropertyName) {
+        ScheduledJobs(final String jobName, final Class<? extends RepositoryJob> jobClass, final String systemPropertyName) {
             this.jobName = jobName;
             this.jobClass = jobClass;
             this.systemPropertyName = systemPropertyName;
-            this.jcrPropertyName = jcrPropertyName;
         }
     }
 }
