378ef72d436d3b5dd0e49389f00c8dab4f8e6a96
Deven <Devan.rawat1@nhs.net>
2021-05-04 10:15:55 +0100

    [DW-2326] Omit suppressed series from CSV download

--- site/components/src/main/java/uk/nhs/digital/freemarker/highcharts/ChartData.java
+++ site/components/src/main/java/uk/nhs/digital/freemarker/highcharts/ChartData.java
@@ -1,5 +1,12 @@
 package uk.nhs.digital.freemarker.highcharts;
 
+import org.apache.commons.lang.StringUtils;
+
+import java.io.UnsupportedEncodingException;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 
@@ -7,8 +14,59 @@ import javax.xml.bind.annotation.XmlAccessorType;
 public class ChartData {
 
     private String data;
+    private String filterData;
+    private String suppressedSeries;
 
     public String getData() {
         return this.data;
+    }
+
+    public String getFilterData() {
+        filterData = data;
+        try {
+            if (StringUtils.isNotBlank(suppressedSeries)) {
+                filterData = removeSeries();
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return filterData;
+    }
+
+    public String removeSeries() throws UnsupportedEncodingException {
+        byte[] decodedBytes = Base64.getDecoder().decode(getData());
+        String decodedString = new String(decodedBytes);
+        String[] split = decodedString.split("\n");
+
+        Map<Integer, List> map = new HashMap<>();
+        String suppressedString = getSuppressedSeries();
+        AtomicInteger i = new AtomicInteger();
+        Arrays.stream(split).forEach(line -> {
+                Pattern.compile(",").splitAsStream(line).forEachOrdered(s -> map.computeIfAbsent(i.get(), ArrayList::new).add(s));
+                i.getAndIncrement();
+            }
+        );
+
+        Arrays.stream(suppressedString.split(",")).forEach(s -> {
+            if (map.get(0).indexOf(s) > -1) {
+                int index = map.get(0).indexOf(s);
+                map.keySet().stream().forEach(ss -> {
+                    map.get(ss).remove(index);
+                });
+            }
+        });
+        String str = map.entrySet().stream()
+            .map(entry -> String.join(",", entry.getValue()))
+            .collect(Collectors.joining("\n")).toString();
+        String base64encodedString = Base64.getEncoder().encodeToString(str.getBytes("utf-8"));
+        return base64encodedString;
+    }
+
+    public String getSuppressedSeries() {
+        return suppressedSeries;
+    }
+
+    public void setSuppressedSeries(String suppressedSeries) {
+        this.suppressedSeries = suppressedSeries;
     }
 }
