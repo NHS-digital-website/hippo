8523e0aa23a04e4152a234f76e7f7449ef0e5c14
Rafał Kociński <rafal.kocinski@infinityworks.com>
2021-05-24 12:17:45 +0100

    [ADZ-439] Revert
    This reverts commit 33f0a8d0 in order to eliminate
    even the slightest possibility of it contributing
    to memory usage and performance which is currently
    a challenge that the Website Team is battling.

--- cms/src/main/java/uk/nhs/digital/apispecs/swagger/SwaggerCodeGenOpenApiSpecificationJsonToHtmlConverter.java
+++ cms/src/main/java/uk/nhs/digital/apispecs/swagger/SwaggerCodeGenOpenApiSpecificationJsonToHtmlConverter.java
@@ -1,28 +1,21 @@
 package uk.nhs.digital.apispecs.swagger;
 
-import static java.util.Objects.requireNonNull;
-
 import io.swagger.codegen.v3.*;
-import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.parser.OpenAPIV3Parser;
-import io.swagger.v3.parser.core.models.ParseOptions;
-import io.swagger.v3.parser.core.models.SwaggerParseResult;
 import org.apache.commons.io.FileUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import uk.nhs.digital.apispecs.OpenApiSpecificationJsonToHtmlConverter;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 public class SwaggerCodeGenOpenApiSpecificationJsonToHtmlConverter implements OpenApiSpecificationJsonToHtmlConverter {
-
-    private static final Logger log = LoggerFactory.getLogger(SwaggerCodeGenOpenApiSpecificationJsonToHtmlConverter.class);
 
     private static final String SPEC_HTML_FILE_NAME = "index.html";
 
@@ -30,75 +23,85 @@ public class SwaggerCodeGenOpenApiSpecificationJsonToHtmlConverter implements Op
 
     @Override public String htmlFrom(final String openApiSpecificationJson) {
 
-        try {
-            final OpenAPI openApiModel = openApiModelFrom(openApiSpecificationJson);
-
-            return toHtml(openApiModel);
-
-        } catch (final Exception e) {
-            throw new RuntimeException("Failed to generate HTML for OpenAPI specification.", e);
-        }
-    }
-
-    private OpenAPI openApiModelFrom(final String openApiSpecificationJson) {
-
-        final ParseOptions parseOptions = new ParseOptions();
-        parseOptions.setResolve(true);
-
-        final SwaggerParseResult swaggerParseResult = new OpenAPIV3Parser()
-            .readContents(openApiSpecificationJson, null, parseOptions);
-
-        Optional.ofNullable(swaggerParseResult.getMessages())
-            .orElse(Collections.emptyList())
-            .forEach(log::warn);
-
-        return swaggerParseResult.getOpenAPI();
-    }
-
-    private String toHtml(final OpenAPI openApi) throws IOException {
-
         File specificationTempOutputDirectory = null;
+
         try {
             specificationTempOutputDirectory = createSpecificationTempOutputDirectory();
 
-            final Generator generator = configureCodeGenWith(openApi, specificationTempOutputDirectory);
+            final File specificationJson = apiSpecificationJsonFileWith(openApiSpecificationJson, specificationTempOutputDirectory);
 
-            final List<File> codegenSpecificationOutputFiles = generator.generate();
+            final List<File> swaggerSpecificationOutputFiles = convertSpecificationJsonToHtml(specificationJson, specificationTempOutputDirectory);
 
-            return specificationHtmlFrom(codegenSpecificationOutputFiles);
+            return specificationHtmlFileFrom(swaggerSpecificationOutputFiles);
 
+        } catch (final Exception e) {
+            throw new RuntimeException("Failed to generate HTML for OpenAPI specification.", e);
         } finally {
             cleanupIfPresent(specificationTempOutputDirectory);
         }
     }
 
-    private Generator configureCodeGenWith(final OpenAPI openApiModel, final File specificationTempOutputDirectory) {
+    private List<File> convertSpecificationJsonToHtml(final File openApiSpecificationJson, final File specificationTempOutputDirectory) {
 
-        final ApiSpecificationStaticHtml2Codegen codegenConfig = new ApiSpecificationStaticHtml2Codegen();
-        codegenConfig.setOutputDir(specificationTempOutputDirectory.getPath());
-        codegenConfig.additionalProperties()
-            .put(CodegenConstants.TEMPLATE_DIR, filesystemPathFromClassPathResource(TEMPLATE_FOLDER_RELATIVE_RESOURCE_PATH));
+        final File swaggerOutputDirectory = createSwaggerOutputDirectoryIn(specificationTempOutputDirectory);
 
-        final ClientOptInput clientOptInput = new ClientOptInput();
-        clientOptInput
-            .opts(new ClientOpts())
-            .config(codegenConfig)
-            .setOpenAPI(openApiModel);
+        final Generator generator = configureSwaggerCodeGen(openApiSpecificationJson, swaggerOutputDirectory);
 
-        return new DefaultGenerator().opts(clientOptInput);
+        return generateSpecificationHtmlFilesWith(generator);
     }
 
-    private String filesystemPathFromClassPathResource(final String resourceClassPath) {
-        return requireNonNull(
-                getClass().getClassLoader().getResource(resourceClassPath), "Classpath resource not found: " + resourceClassPath
-        ).getPath();
+    private List<File> generateSpecificationHtmlFilesWith(final Generator generator) {
+        return Optional.ofNullable(generator.generate())
+            .orElseGet(Collections::emptyList);
+    }
+
+    private Generator configureSwaggerCodeGen(final File openApiSpecificationJson, final File swaggerOutputDirectory) {
+        final ClientOptInput clientOptInput = new ClientOptInput();
+
+        final ClientOpts clientOptions = new ClientOpts();
+
+        final ApiSpecificationStaticHtml2Codegen codegenConfig = new ApiSpecificationStaticHtml2Codegen();
+        codegenConfig.setOutputDir(swaggerOutputDirectory.getPath());
+        codegenConfig.setTemplateDir(TEMPLATE_FOLDER_RELATIVE_RESOURCE_PATH);
+
+        clientOptInput
+            .opts(clientOptions)
+            .config(codegenConfig)
+            .setOpenAPI(new OpenAPIV3Parser().read(openApiSpecificationJson.toURI().toASCIIString(), null, null));
+
+        return new CodegenDefaultGenerator().opts(clientOptInput);
     }
 
     private File createSpecificationTempOutputDirectory() throws IOException {
         return Files.createTempDirectory("cms_apispecification_temp_output_").toFile();
     }
 
-    private String specificationHtmlFrom(final List<File> apiSpecificationOutputFiles) {
+    private File createSwaggerOutputDirectoryIn(final File tempDir) {
+        final Path swaggerCodegenOutputDir = Paths.get(tempDir.getPath(), "swagger_codegen_output");
+
+        try {
+            return Files.createDirectory(swaggerCodegenOutputDir).toFile();
+        } catch (final IOException e) {
+            throw new RuntimeException("Failed to create Swagger Codegen output directory " + swaggerCodegenOutputDir, e);
+        }
+    }
+
+    private File apiSpecificationJsonFileWith(final String openApiSpecJson, final File tempDir) {
+
+        final Path apiSpecJsonFilePath = Paths.get(tempDir.getPath(), "openApiSpecification.json");
+
+        final File apiSpecJsonFile;
+        try {
+            apiSpecJsonFile = Files.createFile(apiSpecJsonFilePath).toFile();
+            FileUtils.write(apiSpecJsonFile, openApiSpecJson, "UTF-8");
+        } catch (final IOException e) {
+            throw new UncheckedIOException("Failed to save API Specification JSON in " + apiSpecJsonFilePath, e);
+        }
+
+        return apiSpecJsonFile;
+    }
+
+    private String specificationHtmlFileFrom(final List<File> apiSpecificationOutputFiles) {
 
         final File indexFile = apiSpecificationOutputFiles.stream()
             .filter(file -> file.getName().equals(SPEC_HTML_FILE_NAME))
