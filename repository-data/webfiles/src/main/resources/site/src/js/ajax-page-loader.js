3fa18be58054c840e66fff3a2d9bda49d1b0bf43
Lee Jacobson <lee.jacobson1@nhs.net>
2021-06-18 16:17:02 +0100

    [DW-2434] Added feed filtering logic [DW-2434] Added feed UI

---
+++ repository-data/webfiles/src/main/resources/site/src/js/ajax-page-loader.js
@@ -0,0 +1,34 @@
+/*
+ * Simple ajax page loader
+ */
+
+export default function(url, elementsToUpdate, data = null) {
+    let options = {
+        method: 'GET',
+    }
+
+    if (data !== null) {
+        options.method = 'POST';
+        options.body = data;
+    }
+
+    return fetch(url, options)
+        .then(response => response.text())
+        .then(content => {
+            const doc = new DOMParser().parseFromString(content, "text/html");
+
+            if (!Array.isArray(elementsToUpdate)) {
+                elementsToUpdate = [elementsToUpdate];
+            }
+            elementsToUpdate.forEach(classToUpdate => {
+                const newContent = doc.querySelector(classToUpdate);
+                const contentEl = document.querySelector(classToUpdate);
+
+                if (newContent && contentEl) {
+                    contentEl.innerHTML = newContent.innerHTML;
+                }
+            });
+
+            return Promise.resolve(doc);
+        });
+}
