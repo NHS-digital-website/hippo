b733110305f9b0b4fcdde2052d7824a82928deec
Lesley Anne Kelly <lesley.kelly2@nhs.net>
2021-06-22 17:55:50 +0100

    [DW-2235] Fix up email context

--- cms/src/main/java/uk/nhs/digital/EmailMonitoringComponent.java
+++ cms/src/main/java/uk/nhs/digital/EmailMonitoringComponent.java
@@ -5,7 +5,6 @@ import org.onehippo.repository.scheduling.RepositoryJobExecutionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Properties;
 import javax.jcr.RepositoryException;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -13,41 +12,46 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 public class EmailMonitoringComponent implements RepositoryJob {
     private static final Logger log = LoggerFactory.getLogger(EmailMonitoringComponent.class);
 
     @Override
     public void execute(final RepositoryJobExecutionContext context) throws RepositoryException {
-        Context initCtx = new InitialContext();
-        Context envCtx = (Context) initCtx.lookup("java:comp/env");
-        Session session = (Session) envCtx.lookup("mail/NHSMail");
-
         try {
-            // Create a default MimeMessage object.
-            MimeMessage message = new MimeMessage(session);
+            Context initCtx = new InitialContext();
+            Context envCtx = (Context) initCtx.lookup("java:comp/env");
+            Session session = (Session) envCtx.lookup("mail/NHSMail");
 
-            // Set From: header field of the header.
-            message.setFrom(new InternetAddress(request.getParameter("from")));
+            try {
+                // Create a default MimeMessage object.
+                MimeMessage message = new MimeMessage(session);
 
-            // Set To: header field of the header.
-            final String to = "emailmonitoring@nhs.net";
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+                // Set From: header field of the header.
+                message.setFrom(new InternetAddress("emailmonitoring@nhs.net"));
 
-            // Set Subject: header field
-            message.setSubject("EMAIL MONITOR ONLY");
+                // Set To: header field of the header.
+                message.addRecipient(Message.RecipientType.TO, new InternetAddress("emailmonitoring@nhs.net"));
 
-            // Now set the actual message
-            message.setText("AUTOMATED EMAIL - eForms Monitoring. Please do not delete.");
+                // Set Subject: header field
+                message.setSubject("EMAIL MONITOR ONLY");
 
-            // Send message
-            Transport.send(message);
-            log.info("EMAIL MONITOR: SUCCESS");
-        } catch (MessagingException mex) {
-            mex.printStackTrace();
-            log.warn("EMAIL MONITOR: FAILED");
-            log.warn(mex.getMessage());
+                // Now set the actual message
+                message.setText("AUTOMATED EMAIL - eForms Monitoring. Please do not delete.");
+
+                // Send message
+                Transport.send(message);
+                log.info("EMAIL MONITOR: SUCCESS");
+            } catch (MessagingException mex) {
+                mex.printStackTrace();
+                log.warn("EMAIL MONITOR: FAILED");
+                log.warn(mex.getMessage());
+            }
+        } catch (NamingException e) {
+            log.warn(e.getMessage());
         }
     }
 }
