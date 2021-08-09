43224effbaa2d27b949bf4b6a3ee17490a29b8ac
Deven Rawat <devan.rawat1@nhs.net>
2021-08-09 10:18:53 +0100

    Fixed

--- site/components/src/main/java/uk/nhs/digital/website/beans/Policy.java
+++ site/components/src/main/java/uk/nhs/digital/website/beans/Policy.java
@@ -2,7 +2,6 @@ package uk.nhs.digital.website.beans;
 
 import org.hippoecm.hst.content.beans.Node;
 import org.hippoecm.hst.content.beans.standard.HippoCompound;
-import org.hippoecm.hst.content.beans.standard.HippoHtml;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 
 @HippoEssentialsGenerated(internalName = "website:corporatepolicy")
@@ -12,5 +11,5 @@ public class Policy extends HippoCompound {
     public String getSectionType() {
         return "corporatepolicy";
     }
-    
+
 }
