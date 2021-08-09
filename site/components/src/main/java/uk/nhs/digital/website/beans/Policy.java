c3f8957072a19021ad50a61d9b0182621a1d96c1
Deven Rawat <devan.rawat1@nhs.net>
2021-08-09 10:01:00 +0100

    [DW-1995] Added Policy docType

---
+++ site/components/src/main/java/uk/nhs/digital/website/beans/Policy.java
@@ -0,0 +1,16 @@
+package uk.nhs.digital.website.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoCompound;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+
+@HippoEssentialsGenerated(internalName = "website:corporatepolicy")
+@Node(jcrType = "website:corporatepolicy")
+public class Policy extends HippoCompound {
+
+    public String getSectionType() {
+        return "corporatepolicy";
+    }
+    
+}
