b6f3c9bb3cb0a33b10471d01b44c965e8da87f51
Deven Rawat <devan.rawat1@nhs.net>
2021-08-10 10:11:26 +0100

    [DW-1996] Policy page doctype

---
+++ site/components/src/main/java/uk/nhs/digital/website/beans/PolicyPage.java
@@ -0,0 +1,16 @@
+package uk.nhs.digital.website.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoCompound;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+
+@HippoEssentialsGenerated(internalName = "website:policypage")
+@Node(jcrType = "website:policypage")
+public class PolicyPage extends HippoCompound {
+
+    public String getSectionType() {
+        return "policypage";
+    }
+
+}
