4766471fa5f69b408061bddb687dfedde0f892bb
Lesley Anne Kelly <lesley.kelly2@nhs.net>
2021-09-03 11:12:07 +0100

    [Dw-1361] Correction post rebrand

--- repository-data/webfiles/src/main/resources/site/src/js/nhsd-frontend-scripts.js
+++ repository-data/webfiles/src/main/resources/site/src/js/nhsd-frontend-scripts.js
@@ -4,10 +4,13 @@ import './utils/public-path';
  * Scripts to load just before `</body>`
  */
 import {initCookieConsent} from "./relevance/relevance-cookie";
+import "./nhsd-frontend/nhsd-frontend";
+import {printingEvents} from "./events/printingEvents";
 import "./print-publication";
 import "./feed-page";
 
 initCookieConsent();
+printingEvents();
 
 if (document.querySelector('[data-chartsource=highchart]')) {
     import(/* webpackChunkName: "highchart-setup" */ './highcharts/highchart-setup').then(module => {
