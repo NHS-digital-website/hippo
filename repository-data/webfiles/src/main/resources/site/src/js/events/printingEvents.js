de4f9fa1deb230847c2ea9f52cf377814aad96d4
Lesley Anne Kelly <lesley.kelly2@nhs.net>
2021-09-15 12:47:16 +0100

    [DW-1361] Add dynamic chart print friendly too

--- repository-data/webfiles/src/main/resources/site/src/js/events/printingEvents.js
+++ repository-data/webfiles/src/main/resources/site/src/js/events/printingEvents.js
@@ -1,14 +1,19 @@
 export function printingEvents() {
     document.addEventListener('DOMContentLoaded', function (event) {
         window.onbeforeprint = (event) => {
-            const svgNode = document.getElementsByClassName("highcharts-root");
-            let i;
-            for (i = 0; i < svgNode.length; i++) {
-                console.log(svgNode[i])
-                svgNode[i].remove();
-            }
-            const svgImage = document.getElementById("chartPrintFriendly");
-            svgImage.style.display = "block";
+            const svgNodes = document.getElementsByClassName("highcharts-root");
+            console.log(svgNodes)
+            Array.from(svgNodes).forEach(function(svgNode){
+                console.log(svgNode)
+                svgNode.remove();
+            })
+
+            const svgImages = document.querySelectorAll("#chartPrintFriendly");
+            console.log(svgImages)
+            Array.from(svgImages).forEach(function(svgImage){
+                console.log(svgImage)
+                svgImage.style.display = "block";
+            })
         };
 
          window.onafterprint = (event) => {
