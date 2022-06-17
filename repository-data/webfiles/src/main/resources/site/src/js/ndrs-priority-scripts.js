9f5169b94b71c79a24ca09ade23fff772c1e7356
GeoffHayward <geoffrey.hayward@nhs.net>
2022-06-17 16:46:51 +0100

    [DW-000] Makes NDRS copy code btn work

---
+++ repository-data/webfiles/src/main/resources/site/src/js/ndrs-priority-scripts.js
@@ -0,0 +1,28 @@
+import './utils/public-path';
+
+/**
+ * Scripts to load in the `<head>`
+ */
+import jQuery from "jquery";
+
+import { initBreadcrumbs } from './breadcrumbs/breadcrumbs';
+import { initAria } from './details-element/aria';
+import { initPolyfill } from './details-element/details-element-polyfill/dist/details-element-polyfill';
+
+import { setCookie, getCookie } from './cookies/cookies';
+
+import "./cookiebot/cookiebot-load";
+import "./utils/js-enabled";
+import "./utils/vanilla-js-utils";
+
+window.jQuery = jQuery;
+window.$ = jQuery;
+window.cookies = {
+    setCookie,
+    getCookie
+}
+window.Papa = require("papaparse");
+
+jQuery(() => initBreadcrumbs());
+initAria();
+initPolyfill();
