49fdaddf6f4c00c77d09c8f5ea112c0e50ce701f
GeoffHayward <geoffrey.hayward@nhs.net>
2022-09-26 11:10:32 +0100

    [DW-000] Testing

--- repository-data/webfiles/src/main/resources/site/src/js/nhsd-priority-scripts.js
+++ repository-data/webfiles/src/main/resources/site/src/js/nhsd-priority-scripts.js
@@ -3,7 +3,7 @@ import './utils/public-path';
 /**
  * Scripts to load in the `<head>`
  */
-import jQuery from "jquery";
+import jQuery from 'jquery';
 
 import { initBreadcrumbs } from './breadcrumbs/breadcrumbs';
 import { initAria } from './details-element/aria';
@@ -11,17 +11,17 @@ import { initPolyfill } from './details-element/details-element-polyfill/dist/de
 
 import { setCookie, getCookie } from './cookies/cookies';
 
-import "./cookiebot/cookiebot-load";
-import "./utils/js-enabled";
-import "./utils/vanilla-js-utils";
+import './cookiebot/cookiebot-load';
+import './utils/js-enabled';
+import './utils/vanilla-js-utils';
 
 window.jQuery = jQuery;
 window.$ = jQuery;
 window.cookies = {
     setCookie,
-    getCookie
-}
-window.Papa = require("papaparse");
+    getCookie,
+};
+window.Papa = require('papaparse')
 
 jQuery(() => initBreadcrumbs());
 initAria();
