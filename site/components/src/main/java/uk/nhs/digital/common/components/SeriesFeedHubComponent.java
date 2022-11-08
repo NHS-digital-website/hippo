d4b70d9d0df66ee0a8573f42aceca91861ebc1bd
lekeDevOps <lesley.kelly2@nhs.net>
2022-11-08 11:38:01 +0000

    [DW-3137] Set up series publication search

---
+++ site/components/src/main/java/uk/nhs/digital/common/components/SeriesFeedHubComponent.java
@@ -0,0 +1,11 @@
+package uk.nhs.digital.common.components;
+
+import org.hippoecm.hst.core.parameters.ParametersInfo;
+import org.onehippo.cms7.essentials.components.EssentialsListComponent;
+import uk.nhs.digital.common.components.info.SeriesFeedHubComponentInfo;
+
+@ParametersInfo(
+    type = SeriesFeedHubComponentInfo.class
+)
+public class SeriesFeedHubComponent extends EssentialsListComponent {
+}
