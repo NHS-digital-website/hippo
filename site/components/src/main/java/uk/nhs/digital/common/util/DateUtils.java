72139d24a711956185f5dcc03cfd162a67db38b9
Lee Richardson <lee.richardson4@nhs.net>
2022-11-17 17:41:50 +0000

    Sets rendered spec expiry on  get and set
    Implemented as per the on-disk cache, but this is not the only cache
    involved so might not behave as expected overall.

---
+++ site/components/src/main/java/uk/nhs/digital/common/util/DateUtils.java
@@ -0,0 +1,18 @@
+package uk.nhs.digital.common.util;
+
+import java.time.Duration;
+import java.time.format.DateTimeParseException;
+
+public class DateUtils {
+    public static Duration durationFromIso(final String durationIso)
+        throws IllegalArgumentException, DateTimeParseException {
+
+        String durationIsoTrimmed = durationIso.trim();
+        try {
+            Duration duration = Duration.parse(durationIsoTrimmed);
+            return duration;
+        } catch (final Exception e) {
+            throw new IllegalArgumentException("Duration is not in a valid ISO-8601 format: \"" + durationIsoTrimmed + "\"", e);
+        }
+    }
+}
