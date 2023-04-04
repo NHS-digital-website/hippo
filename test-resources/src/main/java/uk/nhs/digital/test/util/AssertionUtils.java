291575729f8bd124fcc4499cf521aae6d72f4129
Uzair Haroon <uharoon@live.co.uk>
2023-04-04 12:39:46 +0100

    [ADZ-2719] ADZ-1837 Use a logger to log exceptions in AssertionUtils

--- test-resources/src/main/java/uk/nhs/digital/test/util/AssertionUtils.java
+++ test-resources/src/main/java/uk/nhs/digital/test/util/AssertionUtils.java
@@ -7,8 +7,12 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AssertionUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(AssertionUtils.class);
 
     public static <A extends Annotation> void assertClassHasFieldWithAnnotationWithAttributeValue(
         final Class<?> targetClass,
@@ -60,7 +64,7 @@ public class AssertionUtils {
 
             } catch (final Exception e) {
                 // Exception means we don't have a match and that's all we care about.
-                e.printStackTrace();
+                log.error("",e);
             }
 
             return null;
