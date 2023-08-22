624c051719911bd8611627ca75f683b474ac07be
Christian Parker <christian.parker1@nhs.net>
2023-08-22 11:29:28 +0100

    [AMP-1485] Test for tag filtering

--- acceptance-tests/src/test/java/uk/nhs/digital/ps/test/acceptance/steps/site/SiteSteps.java
+++ acceptance-tests/src/test/java/uk/nhs/digital/ps/test/acceptance/steps/site/SiteSteps.java
@@ -96,6 +96,19 @@ public class SiteSteps extends AbstractSpringSteps {
         sitePage.clickCookieAcceptButton();
     }
 
+    @When("^I click on the \"([^\"]*)\" labelled tag$")
+    public void whenIClickOnTagLabelled(String title) throws Throwable {
+        WebElement element = sitePage.findElementWithTitleAndClass(title, "nhsd-a-tag");
+
+        assertThat("I can find element with title: " + title,
+            element, is(notNullValue()));
+
+        sitePage.clickOnElement(element);
+
+        // Note: this is temporary while we have some pages that don't have the new cookie banner (old RPS style)
+        sitePage.clickCookieAcceptButton();
+    }
+
     @When("^I click on the (?:link|button) named \"([^\"]+)\"$")
     public void whenIClickOnTheLinkNamed(String linkName) throws Throwable {
         WebElement element = sitePage.findLinkWithText(linkName);
