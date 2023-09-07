49d8dc740f61515671bbbb6c20791774b962cff7
GeoffHayward <geoffrey.hayward@nhs.net>
2023-09-07 12:42:42 +0100

    [DW-3449] Adds Request IP address to failed Content Searches

--- site/components/src/main/java/uk/nhs/digital/common/components/SearchComponent.java
+++ site/components/src/main/java/uk/nhs/digital/common/components/SearchComponent.java
@@ -76,6 +76,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.function.Function;
 import java.util.stream.IntStream;
 
 /**
@@ -132,6 +133,19 @@ public class SearchComponent extends CommonComponent {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SearchComponent.class);
 
+
+    private static final Function<HstRequest, String> ipAddress = request -> {
+        String[] headersToTry = {"CF-Connecting-IP", "X-Forwarded-For"};
+        for (String header : headersToTry) {
+            String ipAddress = request.getRequestContext().getServletRequest().getHeader(header);
+            if (ipAddress != null) {
+                return ipAddress;
+            }
+        }
+        // If none of the headers contain an IP address, use the remote address
+        return request.getRequestContext().getServletRequest().getRemoteAddr();
+    };
+
     @Override
     public void doBeforeRender(HstRequest request, HstResponse response) {
 
@@ -184,8 +198,9 @@ public class SearchComponent extends CommonComponent {
                     setCommonSearchRequestAttributes(request, paramInfo);
                 } else {
                     LOGGER.error(
-                        MessageFormat.format("Content Search returned a failure, falling back to HST search. {0}",
-                            HstRequestUtils.getHstRequestContext(request).getServletRequest().toString())
+                        MessageFormat.format("Content Search returned a failure, falling back to HST search. {0} - from {1}",
+                            HstRequestUtils.getHstRequestContext(request).getServletRequest().toString()),
+                            ipAddress.apply(request)
                     );
                     buildAndExecuteHstSearch(request, paramInfo);
                 }
