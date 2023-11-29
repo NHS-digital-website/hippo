a3a916a5119b6ed8733e20910f167de843429076
GeoffreyHayward <geoff@geoffhayward.eu>
2023-11-29 12:02:55 +0000

    [DW-3540] Adds some fixes to the Cucumber

--- acceptance-tests/src/test/java/uk/nhs/digital/ps/test/acceptance/AcceptanceTest.java
+++ acceptance-tests/src/test/java/uk/nhs/digital/ps/test/acceptance/AcceptanceTest.java
@@ -9,9 +9,11 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features",
-        plugin = {"pretty", "html:target/cucumber"}
-        )
+    features = "src/test/resources/features",
+    plugin = { "pretty", "html:target/cucumber-reports" },
+    glue = {"uk.nhs.digital.ps.test.acceptance.config", "uk.nhs.digital.ps.test.acceptance.steps"},
+    monochrome = true
+)
 public class AcceptanceTest {
     // no-op, config class only
 }
