cc39b0a80875edd05c6e58415ccce07b8aedc65d
GeoffreyHayward <geoff@geoffhayward.eu>
2024-02-01 10:34:36 +0000

    [DW-000] Adds logging to JadisPoolFactory

--- site/components/src/main/java/uk/nhs/digital/cache/JedisPoolFactory.java
+++ site/components/src/main/java/uk/nhs/digital/cache/JedisPoolFactory.java
@@ -1,13 +1,24 @@
 package uk.nhs.digital.cache;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 import java.net.URI;
 
 public class JedisPoolFactory {
+
+    private static final Logger logger = LoggerFactory.getLogger(JedisPoolFactory.class);
     public static JedisPool factory(String url, int timeout) {
         URI uri = URI.create(url);
         JedisPool jedisPool = new JedisPool(uri, timeout);
+
+        try (Jedis jedis = jedisPool.getResource()) {
+            logger.info("Connected to Redis at {} says {}", url, jedis.ping());
+        } catch (Exception e) {
+            logger.error("Failed to connect to Redis at {}", url, e);
+        }
 
         return jedisPool;
     }
