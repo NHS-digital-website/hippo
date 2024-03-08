7fb06053213d986e0a4c57d25eaf0a34535b98a2
Christian Parker <christian.parker1@nhs.net>
2024-03-08 12:13:26 +0000

    [AMP-2553] Set node order and remove extra calls from helper

--- site/components/src/main/java/uk/nhs/digital/common/components/catalogue/FacetNavHelperImpl.java
+++ site/components/src/main/java/uk/nhs/digital/common/components/catalogue/FacetNavHelperImpl.java
@@ -1,23 +1,21 @@
 package uk.nhs.digital.common.components.catalogue;
 
-import org.hippoecm.hst.content.beans.standard.HippoBean;
-import org.hippoecm.hst.content.beans.standard.HippoDocumentBean;
-import org.hippoecm.hst.content.beans.standard.HippoFacetNavigationBean;
-import org.hippoecm.hst.content.beans.standard.HippoFolderBean;
+import org.hippoecm.hst.content.beans.standard.*;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class FacetNavHelperImpl implements FacetNavHelper {
-    private final HippoFacetNavigationBean facetNav;
+    private final HippoResultSetBean resultSet;
+    private final Set<String> tagsSet;
 
     public FacetNavHelperImpl(HippoFacetNavigationBean facetNav) {
-        this.facetNav = facetNav;
+        this.resultSet = facetNav.getResultSet();
+        this.tagsSet = getTagsFromFacet(facetNav);
     }
 
     public Set<String> getAllTags() {
-        List<HippoFolderBean> tagFolders = facetNav.getFolders().stream().findFirst().get().getFolders();
-        return tagFolders.stream().map(HippoBean::getDisplayName).collect(Collectors.toSet());
+        return tagsSet;
     }
 
     public List<String> getAllTagsForLink(CatalogueLink link) {
@@ -26,7 +24,7 @@ public class FacetNavHelperImpl implements FacetNavHelper {
     }
 
     private List<String> tagsForDocsWithGivenTitle(String title) {
-        List<HippoDocumentBean> documents = facetNav.getResultSet()
+        List<HippoDocumentBean> documents = resultSet
                 .getDocuments()
                 .stream()
                 .filter(document -> document.getSingleProperty("website:title") != null)
@@ -38,5 +36,10 @@ public class FacetNavHelperImpl implements FacetNavHelper {
                 .filter(spec -> Objects.equals(spec.getSingleProperty("website:title"), title))
                 .map(filteredSpec -> Arrays.stream(filteredSpec.getMultipleProperty("hippotaxonomy:keys")).toArray(String[]::new))
                 .flatMap(stream -> Arrays.stream(stream).sequential()).distinct().collect(Collectors.toList());
+    }
+
+    private Set<String> getTagsFromFacet(HippoFacetNavigationBean facetNav) {
+        List<HippoFolderBean> tagFolders = facetNav.getFolders().stream().findFirst().get().getFolders();
+        return tagFolders.stream().map(HippoBean::getDisplayName).collect(Collectors.toSet());
     }
 }
