5f2fd83dc68043c06b2d25eea97d0ebe0fba417b
Geoffrey Hayward <geoffrey.hayward@nhs.net>
2024-10-16 12:07:54 +0100

    [WSP-68] Refactors API and Service Catalouge to use the same code

---
+++ repository-data/application/src/main/resources/hcm-config/configuration/update/WSP-68-service-catalogue-checkmark.groovy
@@ -0,0 +1,45 @@
+package org.hippoecm.frontend.plugins.cms.admin.updater
+
+import org.hippoecm.repository.util.JcrUtils
+import org.onehippo.repository.update.BaseNodeUpdateVisitor
+
+import javax.jcr.Node
+
+/**
+ * Adds showServiceResult true on each document.
+ */
+class ServiceCatalogueCheckmark extends BaseNodeUpdateVisitor {
+
+    boolean doUpdate(Node node) {
+
+        // Query returns the variant nodes of the document
+        // (which has the 3 variants)
+        try {
+            return updateNode(node)
+        } catch (e) {
+            log.error("Failed to process record.", e)
+        }
+
+        return false
+    }
+
+    boolean updateNode(Node n) {
+
+        JcrUtils.ensureIsCheckedOut(n)
+        def path = n.getPath()
+
+        def nodeType = n.getPrimaryNodeType().getName()
+        if (n.hasProperty("website:showServiceResult")) {
+            log.info("Skipped - Adding website:showServiceResult to node: " + path + " => current node type: " + nodeType)
+            return false
+        } else {
+            log.info("Success - Adding website:showServiceResult to node: " + path + " => current node type: " + nodeType)
+            n.setProperty("website:showServiceResult", true)
+            return true
+        }
+    }
+
+    boolean undoUpdate(Node node) {
+        throw new UnsupportedOperationException('Updater does not implement undoUpdate method')
+    }
+}
---
+++ site/components/src/main/java/uk/nhs/digital/common/components/catalogue/FilteredEssentialsFacetsComponent.java
@@ -0,0 +1,55 @@
+package uk.nhs.digital.common.components.catalogue;
+
+import com.google.common.base.Strings;
+import org.apache.commons.lang3.StringUtils;
+import org.hippoecm.hst.content.beans.standard.HippoFacetNavigationBean;
+import org.hippoecm.hst.core.component.HstRequest;
+import org.hippoecm.hst.core.component.HstResponse;
+import org.hippoecm.hst.core.parameters.ParametersInfo;
+import org.hippoecm.hst.core.request.HstRequestContext;
+import org.hippoecm.hst.core.request.ResolvedSiteMapItem;
+import org.hippoecm.hst.util.ContentBeanUtils;
+import org.hippoecm.hst.util.PathUtils;
+import org.onehippo.cms7.essentials.components.EssentialsFacetsComponent;
+import org.onehippo.cms7.essentials.components.info.EssentialsFacetsComponentInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ParametersInfo(
+    type = EssentialsFacetsComponentInfo.class
+)
+public class FilteredEssentialsFacetsComponent extends EssentialsFacetsComponent {
+
+    private static final Logger log = LoggerFactory.getLogger(FilteredEssentialsFacetsComponent.class);
+
+    @Override
+    public void doBeforeRender(HstRequest request, HstResponse response) {
+        super.doBeforeRender(request, response);
+    }
+
+    @Override
+    protected HippoFacetNavigationBean getFacetNavigationBean(HstRequestContext context, String path, String query) {
+        if (Strings.isNullOrEmpty(path)) {
+            log.warn("Facetpath was empty {}", path);
+            return null;
+        } else {
+            ResolvedSiteMapItem resolvedSiteMapItem = context.getResolvedSiteMapItem();
+            String resolvedContentPath = PathUtils.normalizePath(resolvedSiteMapItem.getRelativeContentPath());
+            String searchQuery = this.cleanupSearchQuery(query);
+
+            if (searchQuery != null && !searchQuery.isEmpty()) {
+                searchQuery = "xpath(//*[jcr:contains(@website:title,'" + searchQuery + "') or jcr:contains(@website:shortsummary,'" + searchQuery + "')])";
+            }
+
+            HippoFacetNavigationBean facNavBean;
+            if (!StringUtils.isBlank(resolvedContentPath) && !resolvedContentPath.startsWith("/")
+                && context.getSiteContentBaseBean().getBean(resolvedContentPath, HippoFacetNavigationBean.class) != null) {
+                facNavBean = ContentBeanUtils.getFacetNavigationBean(resolvedContentPath, searchQuery);
+            } else {
+                facNavBean = ContentBeanUtils.getFacetNavigationBean(path, searchQuery);
+            }
+
+            return facNavBean;
+        }
+    }
+}
