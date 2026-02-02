c19d0a717cdac67f621833ed1aa830311f2db30c
Geoffrey Hayward <geoffrey.hayward@nhs.net>
2026-02-02 14:35:30 +0000

    [WSP-918] Address component thread safety

--- site/components/src/main/java/uk/nhs/digital/common/components/LatestNewsAndEventsComponent.java
+++ site/components/src/main/java/uk/nhs/digital/common/components/LatestNewsAndEventsComponent.java
@@ -4,14 +4,9 @@ import org.hippoecm.hst.component.support.bean.BaseHstComponent;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
 import org.hippoecm.hst.core.request.HstRequestContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import uk.nhs.digital.website.beans.HubNewsAndEvents;
 
 public class LatestNewsAndEventsComponent extends BaseHstComponent {
-
-    public static Logger log = LoggerFactory.getLogger(
-        LatestNewsAndEventsComponent.class);
 
     @Override
     public void doBeforeRender(HstRequest request, HstResponse response) {
