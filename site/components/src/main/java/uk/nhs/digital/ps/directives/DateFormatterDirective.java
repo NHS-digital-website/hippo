7e0b8916fcce205724988280c317669fa92a8661
sankar-shunmuga <smsk@zdvp-std-0106.Corp.Internal>
2026-04-08 17:30:38 +0100

    [WSP-944] Adding Junit test cases for the changes
    [WSP-944] Adding null check during dates conversion
    
    [WSP-944] Added null value check
    
    [WSP-944] Added null value check
    
    [WSP-944] Added null value check
    
    [WSP-944] Added null value check
    
    [WSP-944] Added null value check

--- site/components/src/main/java/uk/nhs/digital/ps/directives/DateFormatterDirective.java
+++ site/components/src/main/java/uk/nhs/digital/ps/directives/DateFormatterDirective.java
@@ -6,7 +6,8 @@ import freemarker.core.Environment;
 import freemarker.template.*;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Map;
 import java.util.TimeZone;
@@ -26,12 +27,10 @@ public class DateFormatterDirective implements TemplateDirectiveModel {
     public static final TimeZone TIME_ZONE = TimeZone.getTimeZone("Europe/London");
 
     private static final String PARAM_NAME = "date";
-    private static final SimpleDateFormat DATE_FORMAT;
 
-    static {
-        DATE_FORMAT = new SimpleDateFormat("dd MMM yyyy");
-        DATE_FORMAT.setTimeZone(TIME_ZONE);
-    }
+    private static final DateTimeFormatter DATE_FORMAT =
+        DateTimeFormatter.ofPattern("dd MMM yyyy")
+            .withZone(ZoneId.of("Europe/London"));
 
     @Override
     public void execute(Environment environment, Map parameters, TemplateModel[] templateModels,
@@ -46,11 +45,22 @@ public class DateFormatterDirective implements TemplateDirectiveModel {
     }
 
     protected String formatDate(final Date dateToFormat) {
-        return DATE_FORMAT.format(dateToFormat);
+
+        if (dateToFormat == null) {
+            return "";
+        }
+        return DATE_FORMAT.format(dateToFormat.toInstant());
     }
 
     protected Date getValueAsDate(final Map parameters, final String paramName) {
-        return ((SimpleDate)parameters.get(paramName)).getAsDate();
+
+        TemplateModel model = (TemplateModel) parameters.get(paramName);
+
+        if (model == null) {
+            return null;
+        }
+
+        return ((SimpleDate) model).getAsDate();
     }
 
     protected void assertRequiredParameterPresent(final Map parameters, final Environment environment, String parameterName) throws TemplateException {
