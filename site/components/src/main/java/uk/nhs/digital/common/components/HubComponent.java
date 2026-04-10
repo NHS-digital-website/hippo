7f139cced613b26c3b1a8379a464200b09c1ab69
Geoffrey Hayward <geoffrey.hayward@nhs.net>
2026-04-10 09:09:43 +0100

    [WSP-1032] Fix hub component extra loging

--- site/components/src/main/java/uk/nhs/digital/common/components/HubComponent.java
+++ site/components/src/main/java/uk/nhs/digital/common/components/HubComponent.java
@@ -1,22 +1,43 @@
 package uk.nhs.digital.common.components;
 
-import org.hippoecm.hst.content.beans.standard.*;
-import org.hippoecm.hst.core.component.*;
+import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.hippoecm.hst.content.beans.standard.HippoDocumentBean;
+import org.hippoecm.hst.core.component.HstRequest;
+import org.hippoecm.hst.core.component.HstResponse;
+import org.hippoecm.hst.core.request.HstRequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.nhs.digital.website.beans.Hub;
 
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class HubComponent extends DocumentChildComponent {
+
+    private static final Logger log = LoggerFactory.getLogger(HubComponent.class);
 
     @Override
     public void doBeforeRender(final HstRequest request, final HstResponse response) {
         super.doBeforeRender(request, response);
 
-        Hub hubDocument = request.getRequestContext().getContentBean(Hub.class);
+        HstRequestContext requestContext = request.getRequestContext();
+        Hub hubDocument = requestContext.getContentBean(Hub.class);
+        if (hubDocument == null) {
+            log.warn("HubComponent invoked without a Hub content bean at {}",
+                requestContext.getResolvedSiteMapItem() != null
+                    ? requestContext.getResolvedSiteMapItem().getPathInfo()
+                    : "unknown path");
+            return;
+        }
+
         //creating a map where key is the component and value is a list of its children
         Map<HippoBean, List<HippoDocumentBean>> componentsMap = new LinkedHashMap<>();
-        for (HippoBean component : hubDocument.getComponentlist()) {
-            componentsMap.put(component, getRelatedDocuments(component));
+        List<HippoBean> componentlist = hubDocument.getComponentlist();
+        if (componentlist != null) {
+            for (HippoBean component : componentlist) {
+                componentsMap.put(component, getRelatedDocuments(component));
+            }
         }
         request.setAttribute("components", componentsMap);
     }
