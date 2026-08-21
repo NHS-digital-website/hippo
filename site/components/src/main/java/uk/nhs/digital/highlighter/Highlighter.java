a33528aae04761cf455692333e604cdfc6b3d417
Geoffrey Hayward <geoffrey.hayward@nhs.net>
2026-08-21 10:58:40 +0100

    [WSP-61] Fix syntax highlighter

--- site/components/src/main/java/uk/nhs/digital/highlighter/Highlighter.java
+++ site/components/src/main/java/uk/nhs/digital/highlighter/Highlighter.java
@@ -3,6 +3,8 @@ package uk.nhs.digital.highlighter;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,18 +22,24 @@ public enum Highlighter {
 
     INSTANCE; // Enum Singleton.
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Highlighter.class);
+
     private static final String POLYFILL = "highlighter/nashorn-polyfill.js";
     private static final String HIGHLIGHTER = "highlighter/highlight-js/highlight.pack.js"; // build from https://highlightjs.org/download/
     private static final ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
 
     static {
         try {
-            engine.eval(loadScript(POLYFILL));
-            engine.eval(loadScript(HIGHLIGHTER));
-            engine.eval("function highlight(source, lang) { return hljs.highlight(lang, source).value }");
+            if (engine == null) {
+                LOGGER.warn("Nashorn script engine is not available. Code blocks will be rendered without syntax highlighting.");
+            } else {
+                engine.eval(loadScript(POLYFILL));
+                engine.eval(loadScript(HIGHLIGHTER));
+                engine.eval("function highlight(source, lang) { return hljs.highlight(lang, source).value }");
+            }
         } catch (ScriptException | IOException e) {
             // As we have control of the scripts this will not happen.
-            e.printStackTrace();
+            LOGGER.warn("Failed to initialise the syntax highlighter. Code blocks will be rendered without syntax highlighting.", e);
         }
     }
 
@@ -43,11 +51,15 @@ public enum Highlighter {
     }
 
     public String paint(final String source, final Language lang) {
+        if (engine == null) {
+            return source;
+        }
+
         try {
             return (String) ((Invocable) engine).invokeFunction("highlight", source, lang.getKey());
         } catch (ScriptException | NoSuchMethodException e) {
             // As we have control of the 'print' function this will not happen.
-            e.printStackTrace();
+            LOGGER.warn("Failed to syntax highlight code block as {}. Rendering without syntax highlighting.", lang.getKey(), e);
             return source;
         }
     }
